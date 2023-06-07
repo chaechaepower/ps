013592cd3fe4b22f3a0b745230aae223af3e0978
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2023-06-07 23:18:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -17,17 +17,7 @@ public class Main {
 			arr[i] = Integer.parseInt(bf.readLine());
 		}
 
-		for (int i = 0; i < N - 1; i++) {
-			int min = i;
-			for (int j = i + 1; j < N; j++) {
-				if (arr[j] < arr[min]) {
-					min = j;
-				}
-			}
-			int temp = arr[min];
-			arr[min] = arr[i];
-			arr[i] = temp;
-		}
+		Arrays.sort(arr);
 
 		for (int val : arr) {
 			sb.append(val).append('\n');
