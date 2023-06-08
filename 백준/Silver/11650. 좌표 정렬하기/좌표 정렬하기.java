7ba67ce6d0f6b5d0beb538c0d05bfb47d0f86de9
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2023-06-09 01:08:44 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 796 ms, Memory: 55200 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,38 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(bf.readLine());
-		
-		int[][]arr=new int[n][2];
-		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[N][2];
+
 		StringTokenizer st;
-		for(int i=0;i<n;i++) {
-			st=new StringTokenizer(bf.readLine());
-			arr[i][0]=Integer.parseInt(st.nextToken());
-			arr[i][1]=Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
-			
-		Arrays.sort(arr,(e1,e2)->{
-			if(e1[0]==e2[0]) return e1[1]-e2[1];
-			else return e1[0]-e2[0];
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[0] == e2[0])
+				return e1[1] - e2[1];
+			else
+				return e1[0] - e2[0];
 		});
-		
-		StringBuilder sb=new StringBuilder();
-		for(int i=0;i<n;i++) {
-			sb.append(arr[i][0]+" "+arr[i][1]).append('\n');
+
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
 		}
+
 		System.out.println(sb);
+
 	}
+
 }
