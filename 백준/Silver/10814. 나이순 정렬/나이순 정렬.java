ef17bffc69c95b89b2b9eef658834e27b80d4c94
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2023-06-11 21:38:20 +0900

    [Silver V] Title: 나이순 정렬, Time: 460 ms, Memory: 41500 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,31 +8,30 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[][] idxAge = new int[N][2];
-		String[] name = new String[N];
+		StringBuilder[] sbArr = new StringBuilder[201];
+
+		for (int i = 0; i < 201; i++) {
+			sbArr[i] = new StringBuilder();
+		}
 
 		StringTokenizer st;
 
 		for (int i = 0; i < N; i++) {
+
 			st = new StringTokenizer(br.readLine());
-			idxAge[i][0] = i;
-			idxAge[i][1] = Integer.parseInt(st.nextToken());
-			name[i] = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
+			String name = st.nextToken();
+			sbArr[age].append(age).append(" ").append(name).append("\n");
+
 		}
 
-		Arrays.sort(idxAge, (e1, e2) -> {
-			if (e1[1] == e2[1])
-				return e1[0] - e2[0];
-			else
-				return e1[1] - e2[1];
-		});
+		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < N; i++) {
-			sb.append(idxAge[i][1]).append(" ").append(name[idxAge[i][0]]).append('\n');
+		for (StringBuilder val : sbArr) {
+			sb.append(val);
 		}
 
 		System.out.println(sb);
