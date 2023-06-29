ed6e12be078a98dc151175862cfd39f82d90538d
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2023-06-30 00:30:25 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -9,14 +9,15 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
 
 		dp = new Integer[N + 1];
 		arr = new int[N + 1];
 
-		for (int i = 0; i < N; i++) {
-			arr[i + 1] = Integer.parseInt(bf.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		dp[0] = 0;
@@ -25,16 +26,19 @@ public class Main {
 		if (N >= 2) {
 			dp[2] = arr[1] + arr[2];
 		}
-		
-		for(int i=3;i<=N;i++) {
-			dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
-		}
-		
-		System.out.println(dp[N]);
 
-		bf.close();
+		System.out.println(recur(N));
+
 	}
 
+	static int recur(int n) {
 
+		if (dp[n] == null) {
+
+			dp[n] = Math.max(recur(n - 3) + arr[n - 1], recur(n - 2)) + arr[n];
+		}
+
+		return dp[n];
+	}
 
 }
