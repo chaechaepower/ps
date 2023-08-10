1e82e425f2102ad3e138917fd287876633c65e3a
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2023-08-11 01:25:06 +0900

    [Bronze I] Title: 2진수 8진수, Time: 340 ms, Memory: 21596 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -12,46 +12,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String N = br.readLine();
 
-		if (N.equals("0")) {
-			System.out.println(0);
-			return;
+		StringBuilder sb = new StringBuilder();
+
+		if (N.length() % 3 == 1) {
+			sb.append(N.charAt(0) - '0');
+		}
+		if (N.length() % 3 == 2) {
+			sb.append(2 * (N.charAt(0) - '0') + N.charAt(1) - '0');
 		}
 
-		// 2 to 10
-
-		List<Integer> oct = new ArrayList<>();
-
-		for (int i = N.length() - 1; i >= 0; i -= 3) {
-			int tmp = 1;
-			String part = null;
-
-			if (i == 0) {
-				part = "00";
-				part += N.substring(i, i + 1);
-			} else if (i == 1) {
-				part = "0";
-				part += N.substring(i - 1, i + 1);
-
-			} else {
-				part = N.substring(i - 2, i + 1);
-			}
-
-			int sum = 0;
-			for (int j = 2; j >= 0; j--) {
-				char c = part.charAt(j);
-
-				if (c == '0') {
-				} else { // c=='1'
-					sum += tmp;
-				}
-				tmp *= 2;
-			}
-			oct.add(sum);
+		for (int i = N.length() % 3; i < N.length(); i += 3) {
+			sb.append(4 * (N.charAt(i) - '0') + 2 * (N.charAt(i + 1) - '0') + N.charAt(i + 2) - '0');
 		}
 
-		for (int i = oct.size() - 1; i >= 0; i--) {
-			System.out.print(oct.get(i));
-		}
+		System.out.println(sb);
 	}
 
 }
