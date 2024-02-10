5dfcb8952c721d284c4309bfc037569404e129e3
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-02-11 02:44:42 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2164 ms, Memory: 81740 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,47 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); // 노드 개수 입력
+	static List<Integer>[] tree;
+	static int[] parent;
+	static boolean[] visited;
 
-		// 트리 구조 표현을 위한 그래프 구현
-		ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
-		for (int i = 0; i < n; i++)
-			tree.add(new ArrayList<>());
+	public static void main(String[] args) throws IOException {
 
-		// 그래프 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		tree = new ArrayList[n + 1];
+		parent = new int[n + 1];
+		visited = new boolean[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			tree[i] = new ArrayList<>();
+		}
+
 		for (int i = 0; i < n - 1; i++) {
-			int n1 = sc.nextInt() - 1;
-			int n2 = sc.nextInt() - 1;
-			tree.get(n1).add(n2);
-			tree.get(n2).add(n1);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			Integer a = Integer.parseInt(st.nextToken());
+			Integer b = Integer.parseInt(st.nextToken());
+
+			tree[a].add(b);
+			tree[b].add(a);
 		}
 
-		boolean[] visited = new boolean[n]; // 방문 여부 확인용 배열
-		int[] parentNode = new int[n]; // 부모 노드 저장 배열
-
-		// BFS
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(0);
-		visited[0] = true;
-		while (!queue.isEmpty()) {
-			int v = queue.remove();
-			for (int node : tree.get(v))
-				if (!visited[node]) {
-					visited[node] = true;
-					queue.add(node);
-					parentNode[node] = v; // 부모 노드 찾은 후 저장
-				}
+		dfs(1);
+		for (int i = 2; i <= n; i++) {
+			System.out.println(parent[i]);
 		}
 
-		// 루트 노드를 제외한 나머지 노드의 부모 노드 출력
-		for (int i = 1; i < n; i++)
-			System.out.println(parentNode[i] + 1);
+	}
+
+	static void dfs(int v) {
+
+		visited[v] = true;
+
+		for (int i = 0; i < tree[v].size(); i++) {
+			int find = tree[v].get(i);
+			if (!visited[find]) {
+				parent[find] = v;
+				dfs(find);
+			}
+		}
+
 	}
 
 }
