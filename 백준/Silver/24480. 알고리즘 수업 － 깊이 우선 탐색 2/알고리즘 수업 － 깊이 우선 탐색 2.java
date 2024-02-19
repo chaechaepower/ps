c3c52b115917e90f94a5b8de8a02c3343e433404
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-02-19 14:43:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 2356 ms, Memory: 107920 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	static List<Integer>[] list;
+	static boolean[] visited;
+	static int[] order;
+	static int cnt;
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+
+		order = new int[n + 1];
+		visited = new boolean[n + 1];
+
+		list = new ArrayList[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			list[a].add(b);
+			list[b].add(a);
+		}
+
+		// 오름차순 정렬
+		for (int i = 1; i <= n; i++) {
+			Collections.sort(list[i]);
+		}
+
+		dfs(r);
+		for (int i = 1; i <= n; i++) {
+			System.out.println(order[i]);
+		}
+
+	}
+
+	static void dfs(int v) {
+
+		cnt++;
+		order[v] = cnt;
+		visited[v] = true;
+
+		List<Integer> l = list[v];
+		for (int i = l.size() - 1; i >= 0; i--) {
+			int x = l.get(i);
+			if (!visited[x]) {
+				dfs(x);
+			}
+		}
+	}
+
+}
