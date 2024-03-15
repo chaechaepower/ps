694002961063c0485016c160a6c5d1246d09dc59
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-03-15 19:24:33 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		System.out.println(fac(n));
+
+	}
+
+	static long fac(int n) {
+
+		if (n == 0) {
+			return 1;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return n * fac(n - 1);
+		}
+	}
+}
