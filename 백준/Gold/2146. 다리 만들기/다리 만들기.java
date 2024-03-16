6b2fdacdb450033b58c8dc3ea18ffd54fe16e956
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-03-16 18:04:44 +0900

    [Gold III] Title: 다리 만들기, Time: 204 ms, Memory: 136008 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,103 +1,119 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main{
-    static int N;
-    static int res = 10001;
-    static int[][] req = new int[101][101];
-    static boolean[][] isIslandCheck = new boolean[101][101];
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
+public class Main {
 
-    static void dfs(int y, int x, int islandNum){
-        isIslandCheck[y][x] = true;
-        req[y][x] = islandNum;
+	static int n;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int min = 10000;
 
-        for(int i=0;i<4;i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
 
-            if(req[ny][nx] != 0 && !isIslandCheck[ny][nx]) dfs(ny,nx,islandNum);
-        }
-    }
+		map = new int[n][n];
+		visited = new boolean[n][n];
 
-    static void enlargeIsland(int[][] map, int islandNum){
-        boolean[][] check = new boolean[101][101];
-        Queue<Island> queue = new LinkedList<>();
+		StringTokenizer st;
 
-        // 해당 섬을 큐에 추가
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(map[i][j] == islandNum){
-                    queue.add(new Island(i,j,0));
-                    check[i][j] = true;
-                }
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        while(!queue.isEmpty()){
-            Island position = queue.poll();
+		// 섬 구분
+		int idx = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j] && map[i][j] != 0) {
+					dfs(i, j, idx);
+					idx++;
+				}
+			}
+		}
 
-            for(int i=0;i<4;i++){
-                int ny = position.y + dy[i];
-                int nx = position.x + dx[i];
-                if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
+		// 섬과 섬 사이 최소 거리
 
-                // 만약 이동한 값이 다른 섬이었을 경우 현재 cnt값을 res와 비교
-                if(map[ny][nx] != islandNum && req[ny][nx] != 0){
-                    if(res > position.cnt && position.cnt != 0) res = position.cnt;
-                }
-                else{
-                    // 이동한 좌표가 바다이고 체크되지 않았을 경우
-                    if(map[ny][nx]==0 && !check[ny][nx]){
-                        check[ny][nx] = true;
-                        queue.add(new Island(ny,nx, position.cnt+1));
-                    }
-                }
-            }
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] != 0) { // 각 섬의 모든 점에 대해 최소거리 조사
+					bfs(i, j, map[i][j]);
+				}
+			}
+		}
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+		System.out.println(min);
 
-        for(int i=0;i<N;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++){
-                req[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	}
 
-        // 같은 섬끼리 그룹핑
-        int islandNum = 1;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!isIslandCheck[i][j] && req[i][j] != 0){
-                    dfs(i,j,islandNum);
-                    islandNum++;
-                }
-            }
-        }
+	static void bfs(int r, int c, int islandNum) {
+		Queue<Point> que = new LinkedList<>();
+		boolean[][] check = new boolean[n][n];
 
-        // 각 섬 별로 육지를 확장시켜 다른 육지와 닿았을 때의 값의 최솟값을 구함
-        for(int i=1;i<=islandNum;i++){
-            int[][] map = req.clone();
-            enlargeIsland(map,i);
-        }
+		que.add(new Point(r, c, 0));
+		check[r][c] = true;
 
-        System.out.println(res);
-    }
+		while (!que.isEmpty()) {
+			Point pos = que.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int pr = pos.r + dr[i];
+				int pc = pos.c + dc[i];
+
+				if (pr < 0 || pr >= n || pc < 0 || pc >= n) {
+					continue;
+				}
+
+				if (map[pr][pc] == 0 && !check[pr][pc]) { // 바다&&방문하지 않음
+					check[pr][pc] = true; // 이거 안하면 메모리 초과 뜸. 큐에 같은 위치가 중복으로 들어갈 수 있음.
+					que.add(new Point(pr, pc, pos.cnt + 1));
+				} else if (map[pr][pc] != islandNum && map[pr][pc] != 0) { // 다른 섬 && 바다가 아님!(이 조건 추가 안해서 답 안나왓ㅜ)
+					if (pos.cnt < min) {
+						min = pos.cnt;
+					}
+					return;
+				}
+			}
+
+		}
+	}
+
+	static void dfs(int r, int c, int islandNum) {
+		visited[r][c] = true;
+		map[r][c] = islandNum;
+
+		for (int i = 0; i < 4; i++) {
+			int pr = r + dr[i];
+			int pc = c + dc[i];
+
+			if (pr < 0 || pr >= n || pc < 0 || pc >= n) {
+				continue;
+			}
+
+			if (map[pr][pc] != 0 && !visited[pr][pc]) {
+				dfs(pr, pc, islandNum);
+			}
+		}
+	}
 }
 
-class Island{
-    int y;
-    int x;
-    int cnt;
-    Island(int y, int x, int cnt){
-         this.y = y;
-         this.x = x;
-         this.cnt = cnt;
-     }
+class Point {
+	int r;
+	int c;
+	int cnt; // 다른 섬으로의 거리
+
+	public Point(int r, int c, int cnt) {
+		this.r = r;
+		this.c = c;
+		this.cnt = cnt;
+	}
 }
