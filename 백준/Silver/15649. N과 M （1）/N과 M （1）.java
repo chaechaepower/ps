b7aec5f399e656937d928ee0a407ff4d3302c231
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-04-01 00:05:27 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 20656 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,35 +1,35 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
 
-	static StringBuilder sb = new StringBuilder();;
 	static int[] arr;
 	static boolean[] visited;
+	static int m;
+	static int n;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-		arr = new int[m]; // 출력 저장 배열
+		arr = new int[m];
 		visited = new boolean[n];
 
-		perm(n, m, 0);
-
+		dfs(0);
 		System.out.println(sb);
 	}
 
-	static void perm(int n, int m, int depth) {
+	public static void dfs(int depth) {
 
 		if (depth == m) {
-			for (int e : arr) {
-				sb.append(e).append(' ');
+			for (int val : arr) {
+				sb.append(val).append(' ');
 			}
 			sb.append('\n');
 			return;
@@ -39,9 +39,10 @@ public class Main {
 			if (!visited[i]) {
 				visited[i] = true;
 				arr[depth] = i + 1;
-				perm(n, m, depth + 1);
+				dfs(depth + 1);
 				visited[i] = false;
 			}
 		}
+
 	}
 }
