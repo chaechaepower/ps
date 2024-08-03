201ad2fd73a99fc80c5c69dc886e4cf74672a4d9
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-08-03 17:42:58 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 18472 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,37 +1,45 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+       
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        int n=Integer.parseInt(st.nextToken());
 
-		br.close();
+        boolean[] prime=new boolean[n+1]; 
 
-		for (int i = M; i <= N; i++) {
-			is_prime(i);
-		}
-	}
+        for(int i=2;i<prime.length;i++){
+            prime[i]=true;
+        }
 
-	public static void is_prime(int num) {
-		if (num < 2) {
-			return;
-		}
+        for(int i=2;i<n+1;i++){
+            while (!prime[i]) {
+                i++;
+                if (i>=n) {
+                    break;
+                }
+            }
+            
+            for(int j=i+i;j<n+1;j+=i){
+                prime[j]=false;
+            }
+        }
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=m;i<=n;i++){
+            if (prime[i]) {
+                sb.append(i).append('\n');
+            }
+        }
 
-		if (num == 2) {
-			System.out.println(num);
-			return;
-		}
-
-		for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (num % i == 0) {
-				return;
-			}
-		}
-
-		System.out.println(num);
-	}
+        System.out.println(sb);
+      
+    }
 }
