3f23f871d1643ad01865faf711f945e6562cfaff
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-08-20 23:03:54 +0900

    [Gold V] Title: 집합의 표현, Time: 340 ms, Memory: 54836 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+    static int[] parent;
+    static StringBuilder sb=new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+       
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+
+        parent=new int[n+1];
+        for(int i=0;i<n+1;i++){
+            parent[i]=i;
+        }
+        
+        for(int i=0;i<m;i++){
+            st=new StringTokenizer(br.readLine());
+            int opt=Integer.parseInt(st.nextToken());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+
+            switch (opt) {
+                case 0:
+                    unionParent(a,b);
+                    break;
+            
+                case 1:
+                    findParent(a,b);
+                    break;  
+
+                default:
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void findParent(int a, int b) {
+       if (getParent(a)==getParent(b)) {
+        sb.append("yes").append('\n');
+       }else{
+        sb.append("no").append('\n');
+       }
+    }
+
+    private static void unionParent(int a, int b) {
+        a=getParent(a);
+        b=getParent(b);
+        
+        if (a<b) {
+            parent[b]=a;
+        }else{
+            parent[a]=b;
+        }
+    }
+
+    private static int getParent(int n){
+        if (n==parent[n]) {
+            return n;
+        }
+        return parent[n]=getParent(parent[n]);
+    }
+}
