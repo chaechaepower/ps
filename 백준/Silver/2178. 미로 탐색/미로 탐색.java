412dba8a82c692eb37885af677ec6b76c6289f97
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-13 17:52:01 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,78 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Point {
-	int c;
-	int r;
+class Pos{
+	int x;
+	int y;
 	int cnt;
-
-	Point(int c, int r, int cnt) {
-		this.c = c;
-		this.r = r;
-		this.cnt = cnt;
+	
+	Pos(int x, int y, int cnt){
+		this.x=x;
+		this.y=y;
+		this.cnt=cnt;
 	}
 }
 
 public class Main {
-	static int n, m;
-
-	static int[][] maze;
-	static boolean[][] visited;
-	static int[] dc = { -1, 0, 0, 1 };
-	static int[] dr = { 0, -1, 1, 0 };
-
+	
+	public static int n;
+	public static int m;
+	public static boolean[][] visited;
+	public static boolean[][] maze;
+	public static int[] xDir= {0,-1,0,1};
+	public static int[] yDir= {1,0,-1,0};
+	
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
-		maze = new int[n + 1][m + 1];
-		visited = new boolean[n + 1][m + 1];
-
-		for (int i = 1; i <= n; i++) {
-			String s = br.readLine();
-			for (int j = 1; j <= m; j++) {
-				maze[i][j] = s.charAt(j - 1) - '0';
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		
+		maze=new boolean[n+1][m+1];
+		visited=new boolean[n+1][m+1];
+		
+		for(int i=1;i<=n;i++) {
+			char[] line=br.readLine().toCharArray();
+			
+			for(int j=1;j<=m;j++) {
+				maze[i][j]=line[j-1]=='1'?true:false;
 			}
 		}
-
-		bfs(1, 1);
-
+		
+		bfs(1,1);
 	}
-
-	private static void bfs(int x, int y) {
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(x, y, 1));
-		visited[x][y] = true;
-
-		while (!q.isEmpty()) {
-			Point p = q.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nc = p.c + dc[i];
-				int nr = p.r + dr[i];
-
-				if (nc < 1 || nr < 1 || nc > n || nr > m)
-					continue;
-
-				if (visited[nc][nr] || maze[nc][nr] == 0)
-					continue;
-
-				if (nc == n && nr == m) {
-					System.out.println(p.cnt + 1);
+	
+	public static void bfs(int x, int y) {
+		Queue<Pos> queue=new LinkedList<>();
+		queue.offer(new Pos(x,y,1));
+		visited[x][y]=true;
+		
+		while(!queue.isEmpty()) {
+			Pos pos=queue.poll();
+			
+			for(int i=0;i<4;i++) {
+				int nextX=pos.x+xDir[i];
+				int nextY=pos.y+yDir[i];
+				
+				if(nextX==n && nextY==m) {
+					System.out.println(pos.cnt+1);
 					return;
 				}
-
-				visited[nc][nr] = true;
-				q.add(new Point(nc, nr, p.cnt + 1));
-
+				
+				if(nextX>=1 && nextX<=n && nextY>=1 && nextY<=m) {
+					if(!visited[nextX][nextY]&&maze[nextX][nextY]) {
+						queue.offer(new Pos(nextX,nextY,pos.cnt+1));
+						visited[nextX][nextY]=true;
+					}
+				}
 			}
 		}
-		System.out.println(-1);
 	}
 }
