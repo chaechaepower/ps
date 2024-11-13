7749369268c3249c4fdf7107e03ef1832afa547f
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-14 08:33:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 544 ms, Memory: 144604 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,56 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+
 public class Main {
-
-	static int vertex;
-	static int edge;
-	static boolean[] visited;
-	static int[][] adj;
-
+	
+	public static boolean[] visited;
+	public static List<Integer>[] edge;
+	
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		vertex = Integer.parseInt(st.nextToken());
-		edge = Integer.parseInt(st.nextToken());
-
-		adj = new int[vertex + 1][vertex + 1];
-		visited = new boolean[vertex + 1];
-
-		for (int i = 0; i < edge; i++) {
-			st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			adj[a][b] = 1;
-			adj[b][a] = 1;
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		
+		visited=new boolean[n+1];
+		edge=new ArrayList[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			edge[i]=new ArrayList<>();
 		}
-
-		int cnt = 0;
-		for (int i = 1; i < vertex + 1; i++) {
-			if (!visited[i]) {
+		
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(br.readLine());
+			int v1=Integer.parseInt(st.nextToken());
+			int v2=Integer.parseInt(st.nextToken());
+			
+			edge[v1].add(v2);
+			edge[v2].add(v1);
+		}
+		
+		int cnt=0;
+		
+		for(int i=1;i<=n;i++) {
+			if(!visited[i]) {
 				dfs(i);
 				cnt++;
 			}
 		}
-
+		
 		System.out.println(cnt);
 	}
-
-	static void dfs(int v) {
-
-		visited[v] = true;
-
-		for (int i = 1; i < vertex + 1; i++) {
-			if (!visited[i] && adj[v][i] == 1) {
-				dfs(i);
+	
+	public static void dfs(int v) {
+		visited[v]=true;
+		
+		for(int e:edge[v]) {
+			if(!visited[e]) {
+				dfs(e);
 			}
 		}
 	}
-
 }
