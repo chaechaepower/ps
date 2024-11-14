49b81c49bfb5f7eeba1404b7cd562b9d0b25928e
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-14 16:02:35 +0900

    [Silver II] Title: 유기농 배추, Time: 92 ms, Memory: 13224 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,67 +1,79 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
+	
+	public static int n,m;
+	public static boolean[][] visited;
+	public static boolean[][] field;
+	public static int[] xDir= {0, -1, 0, 1};
+	public static int[] yDir= {1, 0, -1, 0};
 
-	static boolean[][] visited;
-	static int[][] arr;
-	static int[] dx = { 0, 0, -1, 1 };
-	static int[] dy = { 1, -1, 0, 0 };
-	static int m;
-	static int n;
-
+ 
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		
 		StringTokenizer st;
-
-		int t = Integer.parseInt(br.readLine());
-
-		while (t-- > 0) {
-			st = new StringTokenizer(br.readLine());
-
-			m = Integer.parseInt(st.nextToken());
-			n = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
-
-			arr = new int[m][n];
-			visited = new boolean[m][n];
-
-			while (k-- > 0) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-
-				arr[x][y] = 1;
+		
+		while(t-->0) {
+			
+			st=new StringTokenizer(br.readLine());
+			
+			m=Integer.parseInt(st.nextToken());
+			n=Integer.parseInt(st.nextToken());
+			int k=Integer.parseInt(st.nextToken());
+			
+			visited=new boolean[m][n];
+			field=new boolean[m][n];
+			
+			for(int i=0;i<k;i++) {
+				st=new StringTokenizer(br.readLine());
+				
+				int x=Integer.parseInt(st.nextToken());
+				int y=Integer.parseInt(st.nextToken());
+				
+				field[x][y]=true;
 			}
-
-			int cnt = 0;
-			for (int i = 0; i < m; i++) {
-				for (int j = 0; j < n; j++) {
-					if (!visited[i][j] && arr[i][j] == 1) {
-						dfs(i, j);
+			
+			int cnt=0;
+			
+			for(int i=0;i<m;i++) {
+				for(int j=0;j<n;j++) {
+					if(!visited[i][j] && field[i][j]) {
+						dfs(i,j);
 						cnt++;
 					}
 				}
 			}
+			
 			System.out.println(cnt);
 		}
-
 	}
-
-	static void dfs(int x, int y) {
-
-		visited[x][y] = true;
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny] && arr[nx][ny] == 1) {
-				dfs(nx, ny);
+	
+	public static void dfs(int x, int y) {
+		visited[x][y]=true;
+		
+		for(int i=0;i<4;i++) {
+			int newX=x+xDir[i];
+			int newY=y+yDir[i];
+			
+			if(newX>=0 && newX<m && newY>=0 && newY<n) {
+				if(!visited[newX][newY] && field[newX][newY]) {
+					dfs(newX,newY);
+				}
 			}
 		}
 	}
 }
+
