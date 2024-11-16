749bff32186a9dcf63130cd4056cd94f4fcefb58
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-16 10:32:20 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 13360 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,42 +1,49 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Main {
-	static int[][] arr;
-	static boolean[][] visited;
-	static int dx[] = { -1, 0, 1 };
-	static int dy[] = { -1, 0, 1 };
-	static int w;
-	static int h;
+public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static boolean[][] map;
+	public static boolean[][] visited;
+	public static int[] hDir = { -1, -1, -1, 0, 1, 1, 1, 0 };
+	public static int[] wDir = { -1, 0, 1, 1, 1, 0, -1, -1 };
+	public static int h;
+	public static int w;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		StringTokenizer st;
+
 		while (true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
 
-			if (w == 0 && h == 0) {
+			if (w == 0 & h == 0) {
 				break;
 			}
 
-			arr = new int[h][w];
+			map = new boolean[h][w];
 			visited = new boolean[h][w];
 
 			for (int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < w; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
+					int num = Integer.parseInt(st.nextToken());
+					map[i][j] = num == 1 ? true : false;
 				}
 			}
 
 			int cnt = 0;
-
 			for (int i = 0; i < h; i++) {
 				for (int j = 0; j < w; j++) {
-					if (!visited[i][j] && arr[i][j] == 1) {
+					if (!visited[i][j] && map[i][j]) {
 						dfs(i, j);
 						cnt++;
 					}
@@ -44,28 +51,22 @@ class Main {
 			}
 
 			System.out.println(cnt);
-
 		}
 
 	}
 
-	static void dfs(int x, int y) {
+	public static void dfs(int r, int c) {
+		visited[r][c] = true;
 
-		visited[x][y] = true;
+		for (int i = 0; i < 8; i++) {
+			int newR = r + hDir[i];
+			int newC = c + wDir[i];
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				int nx = x + dx[i];
-				int ny = y + dy[j];
-
-				if (nx == 0 && ny == 0)
-					continue;
-
-				if (nx >= 0 && nx < h && ny >= 0 && ny < w && arr[nx][ny] == 1 && !visited[nx][ny]) {
-					dfs(nx, ny);
+			if (newR >= 0 && newR < h && newC >= 0 && newC < w) {
+				if (!visited[newR][newC] && map[newR][newC]) {
+					dfs(newR, newC);
 				}
 			}
 		}
 	}
-
 }
