6eb59f6ddc9c1c9ae0aaf808728ffe7f60d8ebf0
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-16 18:39:38 +0900

    [Silver III] Title: N과 M (3), Time: 296 ms, Memory: 111736 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,43 +1,44 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] result;
-	static int n;
-	static int m;
-	static StringBuilder sb=new StringBuilder();
+	public static int[] seq;
+	public static boolean[] visited;
+	public static int m;
+	public static int n;
+	public static StringBuilder sb=new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		
-		n=Integer.parseInt(st.nextToken());
-		m=Integer.parseInt(st.nextToken());
-		
-		result=new int[m];
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[n];
+		seq = new int[m];
+
 		dfs(0);
-		
 		System.out.println(sb);
 	}
 
-	static void dfs(int depth) {
-		if(depth==m) {
-			for(int e:result) {
+	public static void dfs(int depth) {
+		if (depth == m) {
+			for (int e : seq) {
 				sb.append(e).append(' ');
 			}
 			sb.append('\n');
 			return;
 		}
-		
-		for(int i=0;i<n;i++) {
-			result[depth]=i+1;
-			dfs(depth+1);
-		}
-		
-	}
 
+		for (int i = 0; i < n; i++) {
+			seq[depth] = i+1;
+			dfs(depth + 1);
+		}
+	}
 }
