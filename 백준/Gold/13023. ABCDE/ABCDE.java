166856c87c8eb072c4ecb59ed8d0e6bbe996d74e
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-16 19:42:29 +0900

    [Gold V] Title: ABCDE, Time: 172 ms, Memory: 20756 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -7,67 +7,60 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int n;
-    public static int m;
+	public static boolean[] visited;
+	public static List<Integer>[] edge;
+	public static int m;
+	public static int n;
+	public static boolean exist;
 
-    public static boolean[] visited;
-    public static List<Integer>[] list;
-    public static boolean exist;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException{
-        
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        n=Integer.parseInt(st.nextToken());
-        m=Integer.parseInt(st.nextToken());
+		visited = new boolean[n];
+		edge = new ArrayList[n];
 
-        list=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            list[i]=new ArrayList<>();
-        }
+		for (int i = 0; i < n; i++) {
+			edge[i] = new ArrayList<>();
+		}
 
-        visited=new boolean[n];
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<m;i++){
-            st=new StringTokenizer(br.readLine());
-            int v1=Integer.parseInt(st.nextToken());
-            int v2=Integer.parseInt(st.nextToken());
+			edge[v1].add(v2);
+			edge[v2].add(v1);
+		}
 
-            list[v1].add(v2);
-            list[v2].add(v1);
-        }
-        
-        for(int i=0;i<n;i++){
-            if(!exist){
-                visited[i]=true;
-                dfs(1,i); 
-                visited[i]=false;
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			if (!exist) {
+				dfs(1, i);
+			}
+		}
 
-        if (exist) {
-            System.out.println(1);
-        }else{
-            System.out.println(0);
-        }
+		if (exist)
+			System.out.println(1);
+		else
+			System.out.println(0);
 
-    }
+	}
 
-    public static void dfs(int depth, int ver){
-        if(depth==5){
-            exist=true;
-            return;
-        }
+	public static void dfs(int depth, int v) {
+		if (depth == 5) {
+			exist = true;
+			return;
+		}
 
-        for(int i=0;i<list[ver].size();i++){
-            int node=list[ver].get(i);
-
-            if (!visited[node]) {
-                visited[node]=true;
-                dfs(depth+1, node);
-                visited[node]=false;            
-            }
-        }
-    }
+		visited[v] = true;
+		for (int e : edge[v]) {
+			if (!visited[e]) {
+				dfs(depth + 1, e);
+			}
+		}
+		visited[v] = false;
+	}
 }
