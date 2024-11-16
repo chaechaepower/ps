0d25293c9115686e8674a53660141764d1483e0f
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-16 09:22:27 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1732 ms, Memory: 93004 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -7,52 +7,50 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static List<Integer>[] tree;
-	static int[] parent;
-	static boolean[] visited;
+	public static List<Integer>[] edge;
+	public static boolean[] visited;
+	public static int target;
+	public static StringBuilder sb = new StringBuilder();
+	public static int[] par;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 
-		tree = new ArrayList[n + 1];
-		parent = new int[n + 1];
+		edge = new ArrayList[n + 1];
 		visited = new boolean[n + 1];
+		par = new int[n + 1];
 
 		for (int i = 1; i <= n; i++) {
-			tree[i] = new ArrayList<>();
+			edge[i] = new ArrayList<>();
 		}
 
+		StringTokenizer st;
 		for (int i = 0; i < n - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			Integer a = Integer.parseInt(st.nextToken());
-			Integer b = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
 
-			tree[a].add(b);
-			tree[b].add(a);
+			edge[v1].add(v2);
+			edge[v2].add(v1);
 		}
 
 		dfs(1);
-		for (int i = 2; i <= n; i++) {
-			System.out.println(parent[i]);
-		}
+		for (int i = 2; i <= n; i++)
+			System.out.println(par[i]);
 
 	}
 
-	static void dfs(int v) {
+	public static void dfs(int start) {
 
-		visited[v] = true;
+		visited[start] = true;
 
-		for (int i = 0; i < tree[v].size(); i++) {
-			int find = tree[v].get(i);
-			if (!visited[find]) {
-				parent[find] = v;
-				dfs(find);
+		for (int e : edge[start]) {
+			if (!visited[e]) {
+				par[e] = start;
+				dfs(e);
 			}
 		}
-
 	}
-
 }
