6e0f0dc26a5cbea8a8ddb4d76f25430c7777ff6b
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2024-11-17 11:41:22 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 19956 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,48 +1,53 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[] arr;
-	static boolean[] visited;
-	static int m;
-	static int n;
-	static StringBuilder sb = new StringBuilder();
+	
+	public static boolean[] visited;
+	public static int m;
+	public static int n;
+	public static int[] arr;
+	public static StringBuilder sb=new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
-		arr = new int[m];
-		visited = new boolean[n];
-
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		
+		visited=new boolean[n];
+		arr=new int[m];
+		
 		dfs(0);
 		System.out.println(sb);
 	}
-
+	
 	public static void dfs(int depth) {
-
-		if (depth == m) {
-			for (int val : arr) {
-				sb.append(val).append(' ');
+		if(depth==m) {
+			for(int e:arr) {
+				sb.append(e).append(' ');
 			}
 			sb.append('\n');
 			return;
 		}
-
-		for (int i = 0; i < n; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				arr[depth] = i + 1;
-				dfs(depth + 1);
-				visited[i] = false;
+		
+		for(int i=0;i<n;i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				arr[depth]=i+1;
+				dfs(depth+1);
+				visited[i]=false;
 			}
 		}
-
 	}
+	
+	
 }
