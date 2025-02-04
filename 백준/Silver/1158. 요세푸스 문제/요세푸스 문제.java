f1a69a2dcf5f008af31932229016fa5f47a7f0c5
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-04 15:48:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 376 ms, Memory: 15012 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,43 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		Queue<Integer> q = new LinkedList<>();
-
-		StringBuilder sb = new StringBuilder();
-		sb.append("<");
-
-		for (int i = 1; i <= N; i++) {
-			q.offer(i);
+	public static void main(String[] args) throws IOException{		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		
+		List<Integer> list=new ArrayList<>();
+		for(int i=1;i<=n;i++) {
+			list.add(i);
 		}
-
-		while (q.size() != 1) {
-
-			for (int i = 0; i < K - 1; i++) {
-				q.offer(q.poll());
+		
+		List<Integer> result=new ArrayList<>();
+		
+		int idx=0;
+		int cnt=1;
+		while(true) {
+			if(cnt==k) {
+				int num=list.remove(idx);
+				result.add(num);
+				cnt=0;
+				if(list.isEmpty()) {
+					break;
+				}else {
+					cnt++;
+					continue;
+				}
 			}
-
-			sb.append(q.poll() + ", ");
-
+			idx=(idx+1)%list.size();
+			cnt++;
 		}
-
-		sb.append(q.poll() + ">");
-
-		System.out.println(sb);
-
+		
+		System.out.print("<");
+		for(int i=0;i<n;i++) {
+			if(i==n-1) {
+				System.out.print(result.get(i));
+			}else {
+				System.out.print(result.get(i)+", ");
+			}
+		}
+		System.out.println(">");
 	}
-
 }
+
+
