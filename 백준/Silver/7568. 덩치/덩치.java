0e4b82f9860fe9fc18acdb57d6a824ffe00bfd2b
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-04 14:25:34 +0900

    [Silver V] Title: 덩치, Time: 68 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+		int[] x=new int[n];
+		int[] y=new int[n];
+		
+		StringTokenizer st;
+		for(int i=0;i<n;i++) {
+			st=new StringTokenizer(br.readLine());
+			x[i]=Integer.parseInt(st.nextToken());
+			y[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuilder sb=new StringBuilder();
+		for(int i=0;i<n;i++) {
+			int cnt=0;
+			for(int j=0;j<n;j++) {
+				if(i!=j) {
+					if(x[i]<x[j] && y[i]<y[j]) {
+						cnt++;
+					}
+				}
+			}
+			sb.append(cnt+1).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
+
+
