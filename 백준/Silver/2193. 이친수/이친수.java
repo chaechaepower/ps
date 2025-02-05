716394a00208c73b1caa9b8d3277dcd49d1d96bb
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-06 00:32:09 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -4,23 +4,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Long[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
 		
-		dp=new Long[n+1][2]; //뒷자리가 0,1의 개수 
+		long[] dp=new long[n+1];
 		
-		dp[1][0]=(long) 0;
-		dp[1][1]=(long) 1;
+		dp[1]=1;
 		
 		for(int i=2;i<=n;i++) {
-			dp[i][0]=dp[i-1][0]+dp[i-1][1];
-			dp[i][1]=dp[i-1][0];
+			dp[i]=dp[i-2]+dp[i-1];
 		}
 		
-		System.out.println(dp[n][0]+dp[n][1]);
+		System.out.println(dp[n]);
 	}
 
 }
