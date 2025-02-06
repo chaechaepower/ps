e74b1f98087cbc6149aba1e49a6ece8c4950a862
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-06 13:56:29 +0900

    [Silver II] Title: 연속합, Time: 180 ms, Memory: 22568 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,54 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[] seq;
-	static Integer[] dp;
+	
+	static Long[] dp; //dp[i]: i를 포함하는 연속된 수의 최대 합 
+	static int[] arr;
+	static Long[] maxDp;
 
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		seq = new int[N];
-		dp = new Integer[N];
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < N; i++) {
-
-			seq[i] = Integer.parseInt(st.nextToken());
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		dp=new Long[n];
+		arr=new int[n];
+		maxDp=new Long[n];
+	
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
-
-		dp[0] = seq[0];
-
-		int max = dp[0];
-
-		for (int i = 1; i < N; i++) {
-
-			int comp = recur(i);
-
-			if (comp > max) {
-				max = comp;
-			}
+		
+		//제거 전 
+		dp[0]=(long) arr[0];
+		maxDp[0]=dp[0];
+		for(int i=1;i<n;i++) {
+			dp[i]=Math.max(dp[i-1]+arr[i], arr[i]);
+			maxDp[i]=Math.max(dp[i], maxDp[i-1]);
 		}
-
+		long max=maxDp[n-1];
+		
 		System.out.println(max);
-
 	}
-
-	static int recur(int n) {
-
-		if (dp[n] == null) {
-
-			dp[n] = seq[n];
-
-			dp[n] = Math.max(dp[n], seq[n] + recur(n - 1));
-		}
-
-		return dp[n];
-
-	}
-
 }
+
