3181dbd5283ca656ee89e5d89d5471aaa350f343
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-06 18:15:17 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 2008 ms, Memory: 111988 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -1,73 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static List<Integer>[] list;
+	static List<Integer>[] graph;
 	static boolean[] visited;
 	static int[] order;
-	static int cnt;
+	static int cnt=0; //방문 순서 카운트 
 	static int n;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-
-		order = new int[n + 1];
-		visited = new boolean[n + 1];
-
-		list = new ArrayList[n + 1];
-
-		for (int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		int r=Integer.parseInt(st.nextToken());
+		
+		graph=new ArrayList[n+1]; //간선 정보. 인접 리스트. 정점의 번호는 1~n 
+		for(int i=1;i<n+1;i++) {
+			graph[i]=new ArrayList<>();
 		}
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			list[a].add(b);
-			list[b].add(a);
+		visited=new boolean[n+1]; //방문 여부 체크.
+		order=new int[n+1]; 
+		
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(br.readLine());
+			int u=Integer.parseInt(st.nextToken());
+			int v=Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
 		}
-
-		// 오름차순 정렬
-		for (int i = 1; i <= n; i++) {
-			Collections.sort(list[i]);
+		
+		for(int i=1;i<n+1;i++) {
+			Collections.sort(graph[i],Collections.reverseOrder());
 		}
-
+		
 		bfs(r);
-		for (int i = 1; i <= n; i++) {
+		
+		for(int i=1;i<n+1;i++) {
 			System.out.println(order[i]);
 		}
-
+		
 	}
 
 	static void bfs(int v) {
-
-		Queue<Integer> que = new LinkedList<>();
-		que.add(v);
-		visited[v] = true;
-		order[v] = ++cnt;
-
-		while (!que.isEmpty()) {
-			int x = que.poll();
-			List<Integer> l = list[x];
-
-			for (int i = l.size() - 1; i >= 0; i--) {
-				int temp = l.get(i);
-				if (!visited[temp]) {
-					que.add(temp);
-					order[temp] = ++cnt;
-					visited[temp] = true;
+		Queue<Integer> queue=new LinkedList<>();
+		queue.offer(v);
+		visited[v]=true;
+		
+		while(!queue.isEmpty()) {
+			int node=queue.poll();
+			order[node]=++cnt;
+			
+			for(int e: graph[node]) {
+				if(!visited[e]) {
+					visited[e]=true;
+					queue.offer(e);
 				}
 			}
 		}
