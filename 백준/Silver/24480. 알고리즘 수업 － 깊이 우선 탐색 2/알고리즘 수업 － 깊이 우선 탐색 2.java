52bb56bf1f3c6ecbae65f44aa66686e0e7ab8b7e
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-06 17:38:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 2068 ms, Memory: 111960 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,67 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static List<Integer>[] list;
-	static boolean[] visited;
-	static int[] order;
-	static int cnt;
+	static List<Integer>[] graph;
+	static int[] visited;
+	static int cnt; //방문 순서 카운트 
 	static int n;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-
-		order = new int[n + 1];
-		visited = new boolean[n + 1];
-
-		list = new ArrayList[n + 1];
-
-		for (int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		int r=Integer.parseInt(st.nextToken());
+		
+		graph=new ArrayList[n+1]; //간선 정보. 인접 리스트. 정점의 번호는 1~n 
+		for(int i=1;i<n+1;i++) {
+			graph[i]=new ArrayList<>();
 		}
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			list[a].add(b);
-			list[b].add(a);
+		visited=new int[n+1]; //방문 순서 저장. 0이면 방문 안함. 1 이상이면 방문함. 
+		
+		for(int i=0;i<m;i++) {
+			st=new StringTokenizer(br.readLine());
+			int u=Integer.parseInt(st.nextToken());
+			int v=Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
 		}
-
-		// 오름차순 정렬
-		for (int i = 1; i <= n; i++) {
-			Collections.sort(list[i]);
+		
+		for(int i=1;i<n+1;i++) {
+			Collections.sort(graph[i],Collections.reverseOrder());//오름차순 방문 위함. 
 		}
-
+		
 		dfs(r);
-		for (int i = 1; i <= n; i++) {
-			System.out.println(order[i]);
+		for(int i=1;i<n+1;i++) {
+			System.out.println(visited[i]);
 		}
-
+		
 	}
 
 	static void dfs(int v) {
-
 		cnt++;
-		order[v] = cnt;
-		visited[v] = true;
-
-		List<Integer> l = list[v];
-		for (int i = l.size() - 1; i >= 0; i--) {
-			int x = l.get(i);
-			if (!visited[x]) {
-				dfs(x);
+		visited[v] = cnt;
+		
+		for(int e:graph[v]) {
+			if(visited[e]==0) { // 길이 있고 방문한 적 없음.
+				dfs(e); 
 			}
 		}
 	}
