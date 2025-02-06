5cd4258d224d0b0a445e0d6542b66839b1cbcb38
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-06 15:10:19 +0900

    [Gold V] Title: 연속합 2, Time: 212 ms, Memory: 30828 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -4,66 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Long[] dp; //dp[i]: i를 포함하는 연속된 수의 최대 합 
-	static Long[] maxDp; //maxDp[i]: i까지의 연속된 수의 최대 합 
+	static Long[] dp1; //dp1[i]: i를 포함하는 연속된 수의 최대 합(왼쪽부터)
+	static Long[] dp2; //dp1[i]: i를 포함하는 연속된 수의 최대 합(오른쪽부터)
 	static int[] arr;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
 		
-		dp=new Long[n];
+		dp1=new Long[n];
+		dp2=new Long[n];
 		arr=new int[n];
-		maxDp=new Long[n];
 		
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
 			arr[i]=Integer.parseInt(st.nextToken());
 		}
 		
-		//제거 전 
-		dp[0]=(long) arr[0];
-		maxDp[0]=dp[0];
+		//제거 전 왼쪽부터 연속 최대 합 구하기 
+		dp1[0]=(long) arr[0];
+		long max=dp1[0];
 		for(int i=1;i<n;i++) {
-			dp[i]=Math.max(dp[i-1]+arr[i], arr[i]);
-			maxDp[i]=Math.max(dp[i], maxDp[i-1]);
+			dp1[i]=Math.max(dp1[i-1]+arr[i], arr[i]);
+			max=Math.max(max, dp1[i]);
 		}
-		long max=maxDp[n-1];
 		
-		//i 제거 후 최댓값(i==n-1과 1은 안구해도 됨.)
-		long tempMax=Long.MIN_VALUE; //연속된 수의 합 중 최대 
-		long seqSum = 0;//현재까지의 연속된 수의 합 
+		//제거 전 오른쪽부터 연속 최대 합 구하기 
+		dp2[n-1]=(long)arr[n-1];
+		for(int i=n-2;i>=0;i--) {
+			dp2[i]=Math.max(dp2[i+1]+arr[i], arr[i]);
+		}
+		
 		for(int i=1;i<n-1;i++) {
 			if(arr[i]<0) {
-				for(int j=i+1;j<n;j++) {
-					if(j==i+1) {
-						if(dp[j-2]+arr[j]>arr[j]) {
-							seqSum=dp[j-2]+arr[j];
-							tempMax=seqSum;
-						}else {
-							break;
-						}
-					}else {
-						if(seqSum+arr[j]> arr[j]) {
-							seqSum=seqSum+arr[j];
-							tempMax=Math.max(tempMax, seqSum);
-						}else {
-							break;
-						}
-					}
-				}
-				
-				if(tempMax>max) {
-					max=tempMax;
-				}
+				max=Math.max(max, dp1[i-1]+dp2[i+1]);
 			}
 		}
+		
 		System.out.println(max);
+		
 	}
 
 }
-
-
-
-
-
