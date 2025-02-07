a0d45d895f679533c0565ce6f1646bc460a93896
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-07 15:52:09 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,46 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static boolean[] visited;
+	static int[] arr;
+	static int n,m;
+	static StringBuilder sb=new StringBuilder();
 
-    public static int n;
-    public static int m;
-
-    public static boolean[] visited;
-    public static int[] arr;
-    public static StringBuilder sb=new StringBuilder();
-
-    public static void main(String[] args) throws IOException{
-        
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-
-        n=Integer.parseInt(st.nextToken());
-        m=Integer.parseInt(st.nextToken());
-
-        visited=new boolean[n];
-        arr=new int[m];
-        
-        dfs(0,0);
-        System.out.println(sb);
-    }
-
-    public static void dfs(int depth, int start){
-        if(depth==m){
-            for(int e:arr){
-                sb.append(e).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=start;i<n;i++){
-            if(!visited[i]){
-                visited[i]=true;
-                arr[depth]=i+1;
-                dfs(depth+1, i+1);
-                visited[i]=false;
-            }
-        }
-
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		
+		visited=new boolean[n];
+		arr=new int[m];
+		
+		dfs(0,0);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int depth, int k) { //depth 깊이에서 탐색, 수 k부터 시작
+		if(depth==m) {
+			for(int e:arr) {
+				sb.append(e).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=k;i<n;i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				arr[depth]=i+1;
+				dfs(depth+1,i+1);
+				visited[i]=false;
+			}
+		}
+	}
 }
+
