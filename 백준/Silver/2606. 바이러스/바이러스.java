2ab9ac96d2bce6eef5b1433f4f3d4d66e94516ce
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-02-19 16:40:46 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,51 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[][] arr;
-	static boolean[] visited;
 	static int n;
-	static int cnt;
-
+	static List<Integer>[] adjList;
+	static boolean[] visited;
+	static int cnt=0;
+	
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		n = Integer.parseInt(br.readLine());
-
-		arr = new int[n + 1][n + 1];
-		visited = new boolean[n + 1];
-
-		int con = Integer.parseInt(br.readLine());
-		;
-
-		while (con-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			arr[a][b] = 1;
-			arr[b][a] = 1;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		n=Integer.parseInt(br.readLine());
+		int m=Integer.parseInt(br.readLine());
+		
+		adjList=new ArrayList[n+1]; //1~n대 컴퓨터에 대한 인접 리스트 
+		visited=new boolean[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			adjList[i]=new ArrayList<>();
 		}
-
+		
+		StringTokenizer st;
+		
+		while(m-->0) {
+			st=new StringTokenizer(br.readLine());
+			
+			int v1=Integer.parseInt(st.nextToken());
+			int v2=Integer.parseInt(st.nextToken());
+			
+			adjList[v1].add(v2);
+			adjList[v2].add(v1);
+		}
+		
 		dfs(1);
-		System.out.println(cnt);
+		System.out.println(cnt-1); //1번 컴퓨터 제외 
 	}
-
+	
 	static void dfs(int v) {
-
-		visited[v] = true;
-
-		for (int i = 1; i <= n; i++) {
-			if (arr[v][i] == 1 && !visited[i]) {
-				cnt++;
-				dfs(i);
+		cnt++;
+		visited[v]=true;
+		
+		for(int node:adjList[v]) {
+			if(!visited[node]) {
+				dfs(node);
 			}
 		}
+		
 	}
-
 }
+
+
