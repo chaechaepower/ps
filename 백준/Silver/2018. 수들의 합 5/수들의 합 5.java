25dcc85121740f8dd055015c095ccba11d259704
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-03-11 02:42:12 +0900

    [Silver V] Title: 수들의 합 5, Time: 144 ms, Memory: 11912 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		int cnt=0;
+		
+		for(int i=1;i<=n;i++) {
+			int start=i;
+			int sum=0;
+			
+			for(int j=start; j<=n; j++) {
+				sum+=j;
+				if(sum>n) {
+					break;
+				}
+				
+				if(sum==n) {
+					cnt++;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
+
+
+
+ 
