4a0e1b12e9eb9218aacc76e7d6e47fb2a469805d
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-03-29 23:07:29 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,57 +2,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.function.*;
+
 
 public class Main {
-		
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
 		int n=Integer.parseInt(br.readLine());
 		
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		List<Integer> list=new ArrayList<>();
-		
-		for(int i=0;i<n;i++) {
-			list.add(Integer.parseInt(st.nextToken()));
+		if(n==2) {
+			System.out.println(0);
+			return;
 		}
 		
-		Collections.sort(list);
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
 		
 		int cnt=0;
 		
-		for(int i=0;i<n;i++) {
-			int target=list.get(i);
-				
-			int p=0;
-			int q=n-1;
+		for(int m=0;m<n;m++) {
+			int i=0;
+			int j=n-1;
+			int target=arr[m];
 			
-			while(p<q) {
-				if(p==i) {
-					p++;
-					if(p>=q) {
-						break;
-					}
-				}
-				if(q==i) {
-					q--;
-					if(q<=p) {
+			while(i<j) {
+				if(i==m) {
+					i++;
+					if(i>=j) {
 						break;
 					}
 				}
 				
-				int sum=list.get(p)+list.get(q);
+				if(j==m) {
+					j--;
+					if(i>=j) {
+						break;
+					}
+				}
+				
+				int sum=arr[i]+arr[j];
 				
 				if(sum<target) {
-					p++;
+					i++;
 				}
+				
 				else if(sum>target) {
-					q--;
+					j--;
 				}
+				
 				else {
 					cnt++;
 					break;
