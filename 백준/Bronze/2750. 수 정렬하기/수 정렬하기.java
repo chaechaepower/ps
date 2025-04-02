6bc0a667b6a01f68d30e95410b49d5da39491eab
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-04-03 00:21:47 +0900

    [Bronze II] Title: 수 정렬하기, Time: 88 ms, Memory: 12696 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,31 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
-		int N = Integer.parseInt(br.readLine());
-
-		boolean[] arr = new boolean[2001];
-
-		for (int i = 0; i < N; i++) {
-			int val = Integer.parseInt(br.readLine());
-			arr[val + 1000] = true;
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
 		}
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i]) {
-				sb.append(i - 1000).append('\n');
+		
+		//선택정렬 
+		for(int i=0;i<n-1;i++) {
+			int minIdx=i;
+			
+			for(int j=i+1;j<n;j++) {
+				if(arr[j]<arr[minIdx]) {
+					minIdx=j;
+				}
 			}
+			
+			int temp=arr[minIdx];
+			arr[minIdx]=arr[i];
+			arr[i]=temp;
 		}
-
+		
+		StringBuilder sb=new StringBuilder();
+		for(int e:arr) {
+			sb.append(e).append('\n');
+		}
+		
 		System.out.println(sb);
-
 	}
-
+	
 }
+
+
+
+
