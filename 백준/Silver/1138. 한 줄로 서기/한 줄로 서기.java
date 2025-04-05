18770d2dd94a42a79de8953713d40c9fc3fa6566
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-04-05 14:59:13 +0900

    [Silver II] Title: 한 줄로 서기, Time: 84 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -5,70 +5,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static int n;
-	public static int[] seq; //줄 선 위치 
-	public static int[] tallerCounts;
-	public static boolean[] visited;
-	public static int[] compare;
-	public static boolean isRightSeq;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n=Integer.parseInt(br.readLine());
+		int n=Integer.parseInt(br.readLine());
 	
-		tallerCounts=new int[n+1]; //idx는 키, value는 해당 키보다 큰 사람 수(왼쪽)
-		
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		
+		//int[] input=new int[n+1];
+		int[] seq=new int[n];
+		
 		for(int i=1;i<=n;i++) {
-			tallerCounts[i]=Integer.parseInt(st.nextToken());
-		}
+			int input=Integer.parseInt(st.nextToken());
 		
-		seq=new int[n];
-		visited=new boolean[n+1];
-		dfs(0);
-		
-	}
-	
-	public static void dfs(int depth) {
-		if(depth==n) {
-			compareTaller();
-			if(isRightSeq) {
-				for(int e:seq) {
-					System.out.print(e+" ");
+			int emptyCount=0;
+			
+			for(int j=0;j<n;j++) {
+				if(emptyCount==input && seq[j]==0) {
+					seq[j]=i;
+					break;
 				}
-			}
-			return;
-		}
-		
-		for(int i=1;i<=n;i++) {
-			if(!visited[i]&&!isRightSeq) {
-				visited[i]=true;
-				seq[depth]=i;
-				dfs(depth+1);
-				visited[i]=false;
+				
+				if(seq[j]==0) {
+					emptyCount++;
+				}
+				
 			}
 		}
-	}
-	
-	public static void compareTaller() {
-		compare=new int[n+1];
 		
-		for(int i=0;i<seq.length;i++) {
-			 for(int j=i-1;j>=0;j--) {
-				 if(seq[j]>seq[i]) {
-					 compare[seq[i]]++;
-				 }
-			 }
-			 if(compare[seq[i]]!=tallerCounts[seq[i]]) {
-				 isRightSeq=false;
-				 return;
-			 }
+		for(int i=0;i<n-1;i++) {
+			System.out.print(seq[i]+" ");
 		}
-		
-		isRightSeq=true;
+		System.out.println(seq[n-1]);
 	}
-
 }
 
