11e59620532360a26a1bae5c4aad5daf07bac171
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-05-08 00:41:33 +0900

    [D2] Title: 패턴 마디의 길이, Time: 77 ms, Memory: 23,936 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 public class Solution {
 
@@ -14,28 +11,18 @@ public class Solution {
 		int testN=Integer.parseInt(br.readLine());
 		
 		for(int t=1;t<=testN;t++) {
-			char[] str=br.readLine().toCharArray();
+			String str=br.readLine();
 			
-			Map<Character,Integer> map=new HashMap<>();
-
-			for(char c:str) {
-				map.put(c, map.getOrDefault(c, 0)+1);
+			for(int i=1;i<=str.length();i++) {
+				String str1=str.substring(0,i);
+				String str2=str.substring(i,i+i);
+				
+				if(str1.equals(str2)) {
+					System.out.println("#"+t+" "+str1.length());
+					break;
+				}
 			}
 			
-			List<Character> keySet=new ArrayList<>(map.keySet());
-
-			keySet.sort((c1,c2)->{
-				return map.get(c1)-map.get(c2);
-			});
-			
-			int candidate=str.length/(map.get(keySet.get(0)));
-			
-			if(str[0]==str[candidate]) {
-				System.out.println("#"+t+" "+candidate);
-			}
-			else {
-				System.out.println("#"+t+" "+(candidate-1));
-			}
 		}
 	}
 }
