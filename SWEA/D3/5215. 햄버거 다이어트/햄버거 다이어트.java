cf2b97580e29a8b42855fec0f256ecfb064282d1
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-05-13 21:54:30 +0900

    [D3] Title: 햄버거 다이어트, Time: 125 ms, Memory: 26,624 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Ingredient {
+	int grade;
+	int cal;
+
+	public Ingredient(int grade, int cal) {
+		this.grade = grade;
+		this.cal = cal;
+	}
+}
+
+public class Solution {
+	static int n;
+	static int lessCal;
+	static Ingredient[] ingredients;
+	static int maxGradeSum;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testN = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= testN; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			n = Integer.parseInt(st.nextToken());
+			lessCal = Integer.parseInt(st.nextToken());
+			ingredients = new Ingredient[n];
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int grade = Integer.parseInt(st.nextToken());
+				int cal = Integer.parseInt(st.nextToken());
+
+				ingredients[i] = new Ingredient(grade, cal);
+			}
+
+			maxGradeSum = Integer.MIN_VALUE;
+
+			dfs(0, 0, 0, 0);
+			System.out.printf("#%d %d\n", t, maxGradeSum);
+		}
+	}
+
+	public static void dfs(int calSum, int gradeSum, int start, int depth) {
+		if (depth > n) {
+			return;
+		}
+
+		else {
+			if (calSum <= lessCal) {
+				if (gradeSum > maxGradeSum) {
+					maxGradeSum = gradeSum;
+				}
+			}
+		}
+
+		for (int i = start; i < n; i++) {
+			dfs(calSum + ingredients[i].cal, gradeSum + ingredients[i].grade, i + 1, depth + 1);
+		}
+	}
+
+}
