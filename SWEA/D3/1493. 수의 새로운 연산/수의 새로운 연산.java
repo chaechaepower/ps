fc8f04f50d49ee5233b700b8ee3ed828772795dd
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-05-24 13:36:28 +0900

    [D3] Title: 수의 새로운 연산, Time: 159 ms, Memory: 30,848 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
@@ -1,54 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static Map<Integer, int[]> map;
-
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testN = Integer.parseInt(br.readLine());
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
-		map=new LinkedHashMap<>();
-		initialize();
-		//arr=new int[][];
+		int testN=Integer.parseInt(br.readLine());
 		
-		for (int t = 1; t <= testN; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			int p = Integer.parseInt(st.nextToken());
-			int q = Integer.parseInt(st.nextToken());
+		for(int t=1;t<=testN;t++) {
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			int p=Integer.parseInt(st.nextToken());
+			int q=Integer.parseInt(st.nextToken());
 			
-			int[] pPos=map.get(p);
-			int[] qPos=map.get(q);
+			int[] pPos=numToPos(p);
+			int[] qPos=numToPos(q);
 			
-			int[] resultPos= {pPos[0]+qPos[0], pPos[1]+qPos[1]};
-			
-			System.out.print("#"+t+" ");
-			for(int i=1;i<=45000;i++) {
-				int[] pos=map.get(i);
-				if(pos[0]==resultPos[0] && pos[1]==resultPos[1]) {
-					System.out.println(i);
-					break;
-				}
-			}
+			int[] sumPos= {pPos[0]+qPos[0],pPos[1]+qPos[1]};
+			int num=posToNum(sumPos[0],sumPos[1]);
+			System.out.printf("#%d %d\n",t,num);
 		}
 	}
 	
-	private static void initialize() {
+	private static int[] numToPos(int num) {
+		if(num==1) {
+			return new int[] {1,1};
+		}
+		
+		int diff=1;
 		int sum=1;
-		int num=1;
 		
-		map.put(0, new int[] {0,0});
-		
-		for(int i=sum;num<=45000;i++) {
-			for(int j=1;j<=i;j++) {
-				map.put(num, new int[] {j,(i+1)-j});
-				num++;
+		while(true) {
+			sum+=diff;
+			diff++;
+			if(num<sum) {
+				diff--;
+				sum-=diff;
+				break;
 			}
 		}
+		
+		int x=1;
+		int y=diff;
+		
+		if(num==sum) {
+			return new int[] {x,y};
+		}
+		
+		for(int i=sum;i<num;i++) {
+			x++;
+			y--;
+		}
+		return new int[] {x,y}; 
+	}
+	
+	private static int posToNum(int x, int y) {
+		int sum=x+y; //1,sum-1부터 시작
+		
+		int start=1;
+		for(int i=1;i<sum-1;i++) {
+			start+=i;
+		}
+		
+		if(x==1) {
+			return start;
+		}
+		
+		int startX=1;
+		int startY=sum-1;
+		int num=start;
+	
+		while(true) {
+			startX++;
+			startY--;
+			num++;
+			if(startY==y) {
+				break;
+			}
+		}
+		
+		return num;
 	}
 }
