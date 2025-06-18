84c2e5d25d0ffa5cf0826313b9721de8637be2e0
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-06-18 21:03:08 +0900

    [Silver IV] Title: ATM, Time: 88 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,39 +1,33 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
-        }
-        
-        //insertion sorting
-        for(int i=1;i<n;i++){
-            for(int j=i;j>0&&arr[j]<arr[j-1];j--){
-                int temp=arr[j];
-                arr[j]=arr[j-1];
-                arr[j-1]=temp;
-            }
-        }
 
-        
-        for(int i=1;i<n;i++){
-            arr[i]=arr[i-1]+arr[i];
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int sum=0;
+		int answer=0;
+		
+		for(int e:arr) {
+			sum+=e;
+			answer+=sum;
+		}
+		
+		System.out.println(answer);
+	}
 
-        int sum=0;
-
-        for(int e:arr)
-            sum+=e;
-
-        System.out.println(sum);
-        
-    }
 }
