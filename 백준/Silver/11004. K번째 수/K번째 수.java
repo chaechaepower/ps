0bfe5fada5d6e523c6dfb6223ae48eb6a1365187
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-06-19 14:18:16 +0900

    [Silver V] Title: K번째 수, Time: 1736 ms, Memory: 501944 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,32 +1,52 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    private static void quickSort(int[] arr, int start, int end) {
+        int part2=partition(arr,start,end);
+        if (start<part2-1) { //왼쪽
+            quickSort(arr,start,part2-1);
+        }
+        if (part2<end) { //오른쪽
+            quickSort(arr,part2,end);
+        }
+    }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int partition(int[] arr, int start, int end) {
+        int pivot=arr[(start+end)/2];
+        while (start<=end) {
+            while(arr[start]<pivot) start++;
+            while(arr[end]>pivot) end--;
+            if (start<=end) {
+                swap(arr,start,end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    private static void swap(int[] arr,int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[N];
-
-		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		Arrays.sort(arr);
-
-		System.out.println(arr[K - 1]);
-
-	}
-
+        int[] arr=new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        quickSort(arr, 0, n-1);
+        System.out.println(arr[k-1]);
+    }
 }
