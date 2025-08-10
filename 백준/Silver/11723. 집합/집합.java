80ff2d61f5bdb2ba4096dc586d2510b0c2e83025
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-08-10 14:10:45 +0900

    [Silver V] Title: 집합, Time: 936 ms, Memory: 325600 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,54 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int m=Integer.parseInt(br.readLine());
-		
+		int n = Integer.parseInt(br.readLine());
+		int s = 0;
+
+		StringTokenizer st;
 		StringBuilder sb=new StringBuilder();
-		
-		int set=0;
-		while(m-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x;
-			
-			switch(st.nextToken()) {
+
+		while (n-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+
+			switch (str) {
 			case "add":
-				x=Integer.parseInt(st.nextToken());
-				set |= (1<<x);
+				int x = Integer.parseInt(st.nextToken());
+
+				if ((s & 1 << x) != 0) {
+					continue;
+				}
+				s |= (1 << x);
+
 				break;
-				
+
 			case "remove":
-				x=Integer.parseInt(st.nextToken());
-				set &= ~(1<<x);
+				x = Integer.parseInt(st.nextToken());
+
+				if ((s & 1 << x) == 0) {
+					continue;
+				}
+
+				s &= ~(1 << x);
+
 				break;
-				
+
 			case "check":
-				x=Integer.parseInt(st.nextToken());
-				sb.append((set & (1<<x)) !=0 ? 1: 0).append('\n');
+				x = Integer.parseInt(st.nextToken());
+
+				if ((s & (1 << x)) != 0) {
+					sb.append(1).append('\n');
+				} else {
+					sb.append(0).append('\n');
+				}
+
 				break;
-				
+
 			case "toggle":
-				x=Integer.parseInt(st.nextToken());
-				set^=(1<<x);
+				x = Integer.parseInt(st.nextToken());
+				s ^= (1 << x);
 				break;
-				
+
 			case "all":
-				set=(1<<21)-1;
+				for (int i = 1; i <= 20; i++) {
+					s |= 1 << i;
+				}
 				break;
-				
+
 			case "empty":
-				set=0;
+				s = 0;
 				break;
 			}
 		}
 		
 		System.out.println(sb);
-		
 	}
-
 }
