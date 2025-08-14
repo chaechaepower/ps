587254e0dbbd6177a59dedf95e99db17d47f1f71
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-08-14 14:04:13 +0900

    [D3] Title: N-Queen, Time: 87 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,51 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
 	static int n;
-	static int[] board;
-	static int answer;
-	static boolean isAnswer = true;
+	static int[] arr;
+	static int ans;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int testN = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= testN; t++) {
 			n = Integer.parseInt(br.readLine());
-			board=new int[n];
+			arr = new int[n];
 
-			answer = 0;
-
+			ans = 0;
 			dfs(0);
-			System.out.printf("#%d %d\n", t, answer);
+			System.out.printf("#%d %d\n", t, ans);
 		}
 	}
 
 	public static void dfs(int depth) {
 		if (depth == n) {
-			answer++;
+			ans++;
 			return;
 		}
 
 		for (int col = 0; col < n; col++) {
-			if(isPossible(depth, col)) {
-				board[depth]=col;
-				dfs(depth+1);
+			if (isPossible(depth, col)) {
+				arr[depth] = col;
+				dfs(depth + 1);
 			}
 		}
 	}
-	
+
 	public static boolean isPossible(int row, int col) {
-		for(int i=0;i<row;i++) {
-			if(board[i]==col || Math.abs(i-row) == Math.abs(board[i]-col) ) {
+		for (int i = 0; i < row; i++) {
+			if (arr[i] == col || Math.abs(i - row) == Math.abs(arr[i] - col)) {
 				return false;
 			}
 		}
 		return true;
 	}
+
 }
