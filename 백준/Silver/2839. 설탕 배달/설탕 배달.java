c87c9701d78c278147d0ff8ebf3fb125117852cb
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-08-15 19:51:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int n = Integer.parseInt(br.readLine());
-
-		int min = 10000000;
-
-		for (int x = 0; x <= n / 3; x++) {
-			for (int y = 0; y <= n / 5; y++) {
-				if (3 * x + 5 * y == n) {
-					if (x + y < min) {
-						min = x + y;
-					}
-				}
+		
+		//3x+5y=n
+		
+		int maxY=n/5;
+		for(int y=maxY;y>=0;y--) {
+			if((n-5*y)%3==0) {
+				int x=(n-5*y)/3;
+				System.out.println(x+y);
+				return;
 			}
 		}
-		if (min == 10000000)
-			min = -1;
-
-		System.out.println(min);
-
+		
+		System.out.println(-1);
 	}
-
 }
