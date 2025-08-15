734126afe2b6f9f414305b59524e7649e35c0a8f
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-08-15 19:05:09 +0900

    [D3] Title: 농작물 수확하기, Time: 93 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -6,38 +6,38 @@ import java.util.StringTokenizer;
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int testN=Integer.parseInt(br.readLine());
-		
-		for(int t=1;t<=testN;t++) {
-			int n=Integer.parseInt(br.readLine());
-			
-			char[][] arr=new char[n][n];
-			
-			StringTokenizer st;
-			for(int i=0;i<n;i++) {
-				arr[i]=br.readLine().toCharArray();
+		int testN = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= testN; t++) {
+			int n = Integer.parseInt(br.readLine());
+			char[][] arr = new char[n][n];
+
+			for (int i = 0; i < n; i++) {
+				arr[i] = br.readLine().toCharArray();
 			}
-			
-			int sum=0;
-			int mid=n/2;
-			
-			for(int i=0;i<=mid;i++) {
-				for(int j=mid-i;j<=mid+i;j++) {
-					sum+=arr[i][j]-'0';
-					if(i==mid) {
-						continue;
-					}
-					sum+=arr[n-1-i][j]-'0';
+
+			int sum = 0;
+
+			int k = 0;
+			for (int i = 0; i < n / 2; i++, k++) {
+				for (int j = n / 2 - k; j <= n / 2 + k; j++) {
+					sum += arr[i][j] - '0';
 				}
 			}
-			
-			System.out.println("#"+t+" "+sum);
-		}
-		
-		
-	}
 
-	
+			for (int j = 0; j < n; j++) {
+				sum += arr[n / 2][j] - '0';
+			}
+
+			k = n / 2 - 1;
+			for (int i = n / 2 + 1; i < n; i++, k--) {
+				for (int j = n / 2 - k; j <= n / 2 + k; j++) {
+					sum += arr[i][j] - '0';
+				}
+			}
+
+			System.out.printf("#%d %d\n", t, sum);
+		}
+	}
 }
