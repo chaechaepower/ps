7f0a1868e12ce5a33c7f50dc6f46be34e839812c
Chaeyoon Kim <109583898+chaechaepower@users.noreply.github.com>
2025-08-18 22:24:25 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 168 ms, Memory: 30,836 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class  Solution{
+public class Solution {
 	static int n;
 	static int[][] arr;
 	static List<int[]> core;
@@ -29,8 +29,9 @@ public class  Solution{
 
 				for (int j = 0; j < n; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
-					
-					if(i<1 || i>n-2 || j<1 || j>n-2) continue;
+
+					if (i < 1 || i > n - 2 || j < 1 || j > n - 2)
+						continue;
 
 					if (arr[i][j] == 1) {
 						core.add(new int[] { i, j });
@@ -40,7 +41,7 @@ public class  Solution{
 
 			cnt = 0;
 			sum = 0;
-			dfs(0,0,0);
+			dfs(0, 0, 0);
 
 			System.out.printf("#%d %d\n", t, sum);
 		}
@@ -51,57 +52,56 @@ public class  Solution{
 			if (nowCnt == cnt) {
 				sum = Math.min(nowSum, sum);
 			} else if (nowCnt > cnt) {
-				cnt=nowCnt;
+				cnt = nowCnt;
 				sum = nowSum;
 			}
 			return;
 		}
 
-		int r=core.get(v)[0];
-		int c=core.get(v)[1];
-		
-		for(int d=0;d<4;d++) {
-			int nr=r;
-			int nc=c;
-			boolean isPass=true;
-			
-			while(true) {
-				nr+=dr[d];
-				nc+=dc[d];
-			
-				if(nr<0 || nr>=n || nc<0 || nc>=n ) {
+		int r = core.get(v)[0];
+		int c = core.get(v)[1];
+
+		for (int d = 0; d < 4; d++) {
+			int nr = r;
+			int nc = c;
+			boolean isPass = true;
+
+			while (true) {
+				nr += dr[d];
+				nc += dc[d];
+
+				if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
 					break;
-				}			
-				
-				if(arr[nr][nc]!=0) {
-					isPass=false;
+				}
+
+				if (arr[nr][nc] != 0) {
+					isPass = false;
 					break;
 				}
 			}
-			
-			if(isPass) {
-				dfs(v+1, nowCnt+1, nowSum+checkWire(r,c,d,2));//0은 빈칸, 1은 코어, 2는 전선
-				checkWire(r,c,d,0);
-			}
-			else {
-				dfs(v+1,nowCnt,nowSum);
+
+			if (isPass) {
+				dfs(v + 1, nowCnt + 1, nowSum + installWire(r, c, d, 2));// 0은 빈칸, 1은 코어, 2는 전선
+				installWire(r, c, d, 0);
 			}
 		}
+		dfs(v + 1, nowCnt, nowSum);
 	}
-	
-	public static int checkWire(int nr, int nc, int d, int type) {
-		int count=0;
-		
-		while(true) {
-			nr+=dr[d];
-			nc+=dc[d];
-			
-			if(nr<0 || nr>=n || nc<0 || nc>=n) break;
-			
-			arr[nr][nc]=type;
+
+	public static int installWire(int nr, int nc, int d, int type) {
+		int count = 0;
+
+		while (true) {
+			nr += dr[d];
+			nc += dc[d];
+
+			if (nr < 0 || nr >= n || nc < 0 || nc >= n)
+				break;
+
+			arr[nr][nc] = type;
 			count++;
 		}
-		
+
 		return count;
 	}
 }
