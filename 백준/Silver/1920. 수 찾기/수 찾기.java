2ffcb3e67c06768a6cf08a01d790831e4040a657
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-08-19 14:46:28 +0900

    [Silver IV] Title: 수 찾기, Time: 516 ms, Memory: 57128 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,61 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-	public static void main(String[] args) throws IOException {
+    static int[] arr;
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       
+        int n=Integer.parseInt(br.readLine());
+        arr=new int[n];
 
-		int[] list = new int[N];
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
 
-		for (int i = 0; i < N; i++) {
-			list[i]=Integer.parseInt(st.nextToken());
-		}
+        int m=Integer.parseInt(br.readLine());
 
-		Arrays.sort(list);
+        st=new StringTokenizer(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<m;i++){
+            int key=Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(0,n-1,key)).append('\n');
+        }
 
-		bf.readLine();
+        System.out.println(sb);
+        
+        
+    }
 
-		st = new StringTokenizer(bf.readLine()); 
-		StringBuilder sb=new StringBuilder();
+    public static int binarySearch(int l, int r, int key){
 
-		while (st.hasMoreTokens()) { 
-			
-			int input = Integer.parseInt(st.nextToken());
-			
-			if(bs(list,input,0,list.length-1)) {
-				sb.append(1).append('\n');
-			}
-			else {
-				sb.append(0).append('\n');
-			}
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static boolean bs(int[] list, int key, int start, int end) {
-		
-		while(start<=end) {
-			int mid=(start+end)/2;
-			
-			if(key==list[mid]) return true;
-			
-			else if(key>list[mid]) return bs(list,key,mid+1,end);
-			
-			else return bs(list,key,start,mid-1);
-		}
-		
-		return false;
-		
-	}
-	
+        while(l<=r){
+            int m=(l+r)/2;
+            
+            if(key<arr[m]) {
+                r=m-1;
+            }
+            else if(key>arr[m]) {
+                l=m+1;
+            }
+            else{ //key==arr[m]
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
