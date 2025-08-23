a3a7b9ca9da7eaeae71cb38fed174c9fcb89b054
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-08-23 21:33:56 +0900

    [Silver I] Title: 스타트와 링크, Time: 252 ms, Memory: 36484 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,88 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n; 
-	static int[][] power;
-	static boolean[] team; //true팀과 false팀으로 구분
-	static int min=Integer.MAX_VALUE; //정답 
-	
+	static int[][] arr;
+	static int n;
+	static boolean[] visited;
+	static int min = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		n=Integer.parseInt(br.readLine());
-		power=new int[n][n];
-		team=new boolean[n];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+
+		arr = new int[n + 1][n + 1];
+
 		StringTokenizer st;
-		
-		for(int i=0;i<n;i++) {
-			st=new StringTokenizer(br.readLine());
-			for(int j=0;j<n;j++) {
-				power[i][j]=Integer.parseInt(st.nextToken());
+		for (int i = 1; i < n + 1; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			for (int j = 1; j < n + 1; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		seperateTeam(0,0);
+
+		visited = new boolean[n + 1];
+		dfs(0, 1);
 		System.out.println(min);
 	}
-	
-	//팀 나누기 
-	static void seperateTeam(int depth, int idx) {
-		
-		if(depth==n/2) {
-			calMin();
+
+	private static void dfs(int depth, int v) {
+		if (depth == n / 2) {
+			int diff = calculatePerformanceDiff();
+			min = Math.min(diff, min);
 			return;
 		}
-		
-		for(int i=idx;i<n;i++) {
-			team[i]=true;
-			seperateTeam(depth+1, i+1);
-			team[i]=false;
-		}
-	}
-	
-	static void calMin() {
-		int trueSum=0;
-		int falseSum=0;
-		
-		List<Integer> teamT=new ArrayList<>();
-		List<Integer> teamF=new ArrayList<>();
-		
-		//팀 분리 
-		for(int i=0;i<n;i++) {
-			if(team[i]) {
-				teamT.add(i);
-			}else {
-				teamF.add(i);
+
+		for (int i = v; i <= n; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				dfs(depth + 1, i + 1);
+				visited[i] = false;
 			}
 		}
-		
-		trueSum=calPower(teamT);
-		falseSum=calPower(teamF);
-		int diff=Math.abs(trueSum - falseSum);
-		
-		min=Math.min(min, diff);
 	}
-	
-	//팀 능력치 계산
-	static int calPower(List<Integer> team) {
-		int sum=0;
-		
-		for(int i=0;i<team.size()-1;i++) {
-			for(int j=i+1;j<team.size();j++) {
-				int t1=team.get(i);
-				int t2=team.get(j);
-				sum+=power[t1][t2];
-				sum+=power[t2][t1];
+
+	private static int calculatePerformanceDiff() {
+		int[] start = new int[n / 2];
+		int[] link = new int[n / 2];
+
+		int s = 0;
+		int l = 0;
+		for (int i = 1; i < n + 1; i++) {
+			if (visited[i]) start[s++] = i;
+			else link[l++] = i;
+		}
+
+		int startSum = calculatePerformance(start);
+		int linkSum = calculatePerformance(link);
+
+		return Math.abs(startSum - linkSum);
+	}
+
+	private static int calculatePerformance(int[] team) {
+		int sum = 0;
+
+		for (int i = 0; i < n / 2 - 1; i++) {
+			int v1 = team[i];
+			for (int j = i + 1; j < n / 2; j++) {
+				int v2 = team[j];
+				
+				sum += arr[v1][v2];
+				sum += arr[v2][v1];
 			}
 		}
 		
 		return sum;
 	}
-	
 }
+
+/*
+ * 1. 팀 나눈다. 2. 각 팀의 능력치의 합을 구한다. 3. 최소 차이를 구한다.
+ */
