217a39d3eb37caf58b703523ddc08a5cb7e5d0cf
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-08-27 15:23:23 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 429 ms, Memory: 110,748 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,56 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static class Edge implements Comparable<Edge> {
-		int from, to;
-		double weight;
+	static class Vertex implements Comparable<Vertex> {
+		int n;
+		double w;
 
-		public Edge(int from, int to, double weight) {
+		public Vertex(int n, double w) {
 			super();
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
+			this.n = n;
+			this.w = w;
 		}
 
 		@Override
-		public int compareTo(Solution.Edge o) {
-			return Double.compare(this.weight, o.weight);
+		public int compareTo(Solution.Vertex o) {
+			return (int) (this.w - o.w);
 		}
-	}
-
-	static int n;
-	static Edge[] edges;
-	static int[] parents;
-
-	static void make() {
-		for (int i = 1; i < n; i++) {
-			parents[i] = i;
-		}
-	}
-
-	static int find(int a) {
-		if (parents[a] == a)
-			return a;
-		return parents[a] = find(parents[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if (aRoot == bRoot)
-			return false;
-
-		if (aRoot < bRoot)
-			parents[bRoot] = aRoot;
-		else
-			parents[aRoot] = bRoot;
-
-		return true;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -58,7 +30,8 @@ public class Solution {
 		int testN = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= testN; t++) {
-			n = Integer.parseInt(br.readLine()); // 섬의 개수
+			int n = Integer.parseInt(br.readLine());
+
 			int[] x = new int[n];
 			int[] y = new int[n];
 
@@ -74,41 +47,56 @@ public class Solution {
 
 			double tax = Double.parseDouble(br.readLine());
 
-			edges = new Edge[n * (n - 1) / 2];
-			parents = new int[n];
-
-			int idx = 0;
+			// 점 사이 거리 계산
+			List<Vertex>[] list = new ArrayList[n];
+			for (int i = 0; i < n; i++)
+				list[i] = new ArrayList<>();
 
 			for (int i = 0; i < n - 1; i++) {
 				for (int j = i + 1; j < n; j++) {
-					int x1 = x[i], y1 = y[i];
-					int x2 = x[j], y2 = y[j];
-					double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+					int x1 = x[i];
+					int x2 = x[j];
+					int y1 = y[i];
+					int y2 = y[j];
 
-					edges[idx++] = new Edge(i, j, Math.pow(distance, 2));
+					double edge = Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+
+					list[i].add(new Vertex(j, edge));
+					list[j].add(new Vertex(i, edge));
 				}
 			}
 
-			Arrays.sort(edges);
-			make();
-
-			int cnt = 0;
+			boolean[] visited=new boolean[n];
+			double[] minEdge=new double[n];
+			Arrays.fill(minEdge, Double.MAX_VALUE);
+			int cnt=0;
 			double result = 0;
+			PriorityQueue<Vertex> pq = new PriorityQueue<>();
+			pq.offer(new Vertex(0, 0));
 
-			for (Edge edge : edges) {
-				if (!union(edge.from, edge.to))
-					continue;
-
+			while (!pq.isEmpty()) {
+				// 1. 비트리 정점 중 최소 간선 정점 구함
+				Vertex cur=pq.poll();
+				if(visited[cur.n]) continue;
+				
+				visited[cur.n]=true;
+				result+=cur.w;
 				cnt++;
-				result += edge.weight;
-
-				if (cnt == n - 1)
-					break;
+				if(cnt==n) break;
+				
+				// 2. 해당 정점과 연결된 정점의 최소 간선 길이 갱신 
+				for(int i=0;i<list[cur.n].size();i++) {
+					Vertex vertex=list[cur.n].get(i);
+					if(visited[vertex.n]) continue;
+					
+					if(vertex.w<minEdge[vertex.n]) {
+						minEdge[vertex.n]=vertex.w;
+						pq.offer(new Vertex(vertex.n,vertex.w));
+					}
+				}
 			}
-
-			double ans=result * tax;
-			System.out.printf("#%d %d\n",t,Math.round(ans));
+			
+			System.out.printf("#%d %d\n",t,Math.round(result*tax));
 		}
-
 	}
 }
