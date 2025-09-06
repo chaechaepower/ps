11498e7a889971ebd6c8b4c591763d96f7ef80a2
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-09-06 18:55:43 +0900

    [Gold IV] Title: 타일 채우기, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -1,27 +1,31 @@
-import java.io.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-    private static int N;
-    private static int[] cache;
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
- 
-        cache = new int[N + 1];
-        if (N % 2 == 0) {
-            cache[0] = 1;
-            cache[2] = 3;
-            for (int i = 4; i <= N; i += 2) {
-                cache[i] = cache[i - 2] * cache[2];
- 
-                for (int j = i - 4; j >= 0; j -= 2) {
-                    cache[i] += (cache[j] * 2);
-                }
-            }
-        }
- 
-        System.out.println(N % 2 == 0 ? cache[N] : 0);
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		
+		if(n%2!=0 || n<2) {
+			System.out.println(0);
+			return;
+		}
+		
+		int[] dp=new int[n+1];
+		dp[2]=3;
+		
+		for(int i=4;i<=n;i+=2) {
+			dp[i]=dp[i-2]*dp[2]+2;
+			
+			for(int j=i-4;j>=2;j-=2) {
+				dp[i]+=dp[j]*2;
+			}
+		}
+		
+		System.out.println(dp[n]);
+	}
 }
