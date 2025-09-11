ccc777be9a710f2c4052625abf246b5c8151b72c
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-09-11 09:44:51 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,419 ms, Memory: 97,712 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int n, adj[][];
+	static int count; // 자신보다 키가 크거나 작은 학생 수
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testN = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= testN; t++) {
+			n=Integer.parseInt(br.readLine());
+			int m=Integer.parseInt(br.readLine());
+			
+			adj=new int[n+1][n+1];// 학생 번호가 1번부터
+			
+			for(int i=0;i<m;i++) {
+				StringTokenizer st=new StringTokenizer(br.readLine());
+				int a=Integer.parseInt(st.nextToken());
+				int b=Integer.parseInt(st.nextToken());
+				adj[a][b]=1; // a보다 b가 크다
+			}
+			
+			int ans=0;
+			for(int i=1;i<n+1;i++) { // 모든 학생에 대해 자신보다 키가 큰 학생 탐색, 작은 학생 탐색 
+				count=0;
+				boolean[] visited=new boolean[n+1];
+				gtDFS(i,visited); // i학생보다 큰 학생 수가 count에 저장됨.
+				ltDFS(i,visited); // i학생보다 작은 학생 수가 count에 저장됨.
+				if(count==n-1) ++ans;
+			}
+			
+			System.out.printf("#%d %d\n",t,ans);
+		}
+	}
+	
+	private static void gtDFS(int cur, boolean[] visited) { // 나보다 키가 큰 학생 탐색 
+		visited[cur]=true;
+		
+		for(int i=1;i<n+1;i++) {
+			if(adj[cur][i]==1 && !visited[i]) { // 방문했던 곳 다시 돌아올 필요 없으므로 방문 여부 검사 
+				++count; // 나보다 큰 애를 볼 때마다 1씩 증가 
+				gtDFS(i,visited);
+			}
+		}
+	}
+
+	private static void ltDFS(int cur, boolean[] visited) { // 나보다 키가 큰 학생 탐색 
+		visited[cur]=true;
+		
+		for(int i=1;i<n+1;i++) {
+			if(adj[i][cur]==1 && !visited[i]) { 
+				++count; // 나보다 작은 애를 볼 때마다 1씩 증가 
+				ltDFS(i,visited);
+			}
+		}
+	}
+}
