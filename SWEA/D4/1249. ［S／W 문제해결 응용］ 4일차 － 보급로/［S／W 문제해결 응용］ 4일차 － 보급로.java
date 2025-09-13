993d884e7afb7723b54b6ddd109b8bce65ed6f8a
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-09-13 18:11:26 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 121 ms, Memory: 30,464 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -6,77 +6,78 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution {
-	static int[][] board;
-	static int n;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testN = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= testN; t++) {
-			n = Integer.parseInt(br.readLine());
-			board = new int[n][n];
+			int n = Integer.parseInt(br.readLine());
+
+			int[][] board = new int[n][n];
 
 			for (int i = 0; i < n; i++) {
 				String line = br.readLine();
+
 				for (int j = 0; j < n; j++) {
 					board[i][j] = line.charAt(j) - '0';
 				}
 			}
-			
-			System.out.printf("#"+t+" "+Dikstra()+"\n");
-		}
-	}
 
-	private static int Dikstra() {
-		int[][] minCost = new int[n][n];
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				minCost[i][j]=Integer.MAX_VALUE;
-			}
-		}
-		minCost[0][0] = board[0][0];
+			/*
+			 * 다익스트라 1. 미방문 정점 중 최단 비용인 정점을 선택 2. 해당 정점과 연결된 미방문 정점의 최단 비용을 갱신(경유지 거침 vs 기존
+			 * 비용)
+			 */
 
-		PriorityQueue<int[]> pq = new PriorityQueue(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[2] - o2[2];
-			}
-		});
+			// 방문 배열, 최소 비용 배열
+			boolean[][] visited = new boolean[n][n];
 
-		pq.offer(new int[] { 0, 0, board[0][0] });
+			int[][] minList = new int[n][n];
+			for(int i=0;i<n;i++) Arrays.fill(minList[i], Integer.MAX_VALUE);
+			minList[0][0] = board[0][0];
 
-		while (!pq.isEmpty()) {
-			// 최소 시간이 가장 작은 정점 선택
-			int[] cur = pq.poll();
-
-			// 그 정점과 연결된 비방문 정점의 최소 시간 갱신 
-			for(int d=0;d<4;d++) {
-				int nr=cur[0]+dr[d];
-				int nc=cur[1]+dc[d];
-				
-				if(nr<0 || nr>=n || nc<0 || nc>=n) continue;
-				
-				if(nr==n-1 && nc==n-1) {
-					return cur[2];
+			PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() { // 0: r, 1:c, 1: 최소 비용
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return o1[2] - o2[2];
 				}
-				
-				if(cur[2]+board[nr][nc]<minCost[nr][nc]) { //가본적 없거나 경유지 거쳤을 때 더 시간이 더 짧다면 
-					minCost[nr][nc]=cur[2]+board[nr][nc];
-					pq.offer(new int[] { nr, nc, minCost[nr][nc] });
+			});
+			pq.offer(new int[] {0,0,board[0][0]});
+
+			int ans = -1;
+
+			while (!pq.isEmpty()) {
+				int[] cur = pq.poll();
+				int curR = cur[0];
+				int curC = cur[1];
+
+				if (visited[curR][curC]) continue;
+
+				if (curR == n - 1 && curC == n - 1) {
+					ans = minList[curR][curC];
+					break;
+				}
+
+				visited[curR][curC] = true;
+
+				for (int d = 0; d < 4; d++) {
+					int nr = curR + dr[d];
+					int nc = curC + dc[d];
+					
+					if(nr<0 || nr>= n || nc<0 || nc>=n) continue;
+
+					if (visited[nr][nc]) continue;
+
+					if (minList[nr][nc] > minList[curR][curC] + board[nr][nc]) {
+						minList[nr][nc] = minList[curR][curC] + board[nr][nc];
+						pq.offer(new int[] { nr, nc, minList[nr][nc] });
+					}
 				}
 			}
+
+			System.out.printf("#%d %d\n", t, ans);
 		}
-		
-		return -1;
 	}
 }
-
-/*
- * S->G로 가는 최소 비용 목적지까지 가는데 마주하는 정점까지의 최소비용을 구하면 됨.
- * 
- * 
- */
