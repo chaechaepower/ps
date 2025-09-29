1b114d95231f92d1f19a92a6f710a39b3c5b79a5
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-09-29 22:44:09 +0900

    [Silver III] Title: 01타일, Time: 84 ms, Memory: 15688 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dp=new int[n+1];
+		dp[1]=1; 
+		if(n==1) {
+			System.out.println(dp[1]);
+			return;
+		}
+		dp[2]=2;
+		
+		for(int i=3;i<=n;i++) {
+			dp[i]=(dp[i-2]+dp[i-1])%15746;
+		}
+		
+		System.out.println(dp[n]%15746);
+	}
+}
