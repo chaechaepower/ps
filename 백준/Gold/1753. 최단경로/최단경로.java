37c5d5c35053b6b0780030654fad58da989ced74
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-11-04 15:23:42 +0900

    [Gold IV] Title: 최단경로, Time: 1812 ms, Memory: 324212 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,87 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    static int N, E;
+    static ArrayList<Node>[] list;
 
-	static class Node implements Comparable<Node>{
-		int to, weight;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); 
+        E = sc.nextInt();
+        int start = sc.nextInt(); 
+        list = new ArrayList[N + 1];
+        for(int i = 0; i <= N; i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < E; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            addGraph(x, y, z);
+        }  
+        boolean[] isVisited = new boolean[N + 1];
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
 
-		public Node(int to, int weight) {
-			super();
-			this.to = to;
-			this.weight = weight;
-		}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.add(new int[]{start, 0});
+        while(!pq.isEmpty()){
+            int[] now = pq.poll();
+            int curNode = now[0];
+            int curDist = now[1];
+            if(isVisited[curNode]) continue;
+            isVisited[curNode] = true;
 
-		@Override
-		public int compareTo(Main.Node o) {
-			return this.weight-o.weight;
-		}
-	}
+            for(int i = 0; i < list[curNode].size(); i++){
+                Node next = list[curNode].get(i);
+                int n = next.to;
+                int w = next.weight;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(br.readLine());
+                if(dist[n] > curDist + w){
+                    dist[n] = curDist + w;
+                    pq.add(new int[]{n, dist[n]});
+                }
+            }
+        }
+        for(int i = 1; i <= N; i++){
+            if(dist[i] == Integer.MAX_VALUE){
+                System.out.println("INF");
+            }else {
+            System.out.println(dist[i]);
+            } 
+        } 
+    }
 
-		List<Node>[] adjList = new ArrayList[V + 1]; // 각 정점으로의 최단거리
-		for (int i = 1; i < V + 1; i++)
-			adjList[i] = new ArrayList<>();
-		int[] minCost = new int[V + 1]; // 간선 비용
-		boolean[] visited = new boolean[V + 1]; // 방문 여부
+    static class Node{
+        int to, weight;
+        public Node(int to, int weight){
+            this.to = to;
+            this.weight = weight;
+        }
+    }
 
-		while (E-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-
-			adjList[u].add(new Node(v, w));
-		}
-
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(K, 0));
-		Arrays.fill(minCost, Integer.MAX_VALUE);
-		minCost[K]=0;
-
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
-
-			if (visited[cur.to])
-				continue;
-			visited[cur.to] = true;
-
-			for (Node next : adjList[cur.to]) {
-				if (visited[next.to])
-					continue;
-
-				if (minCost[next.to] > cur.weight + next.weight) {
-					minCost[next.to] = cur.weight + next.weight;
-					pq.offer(new Node(next.to, minCost[next.to]));
-				}
-			}
-		}
-		
-		
-		for(int i=1;i<V+1;i++) {
-			System.out.println(minCost[i]==Integer.MAX_VALUE? "INF":minCost[i]);
-		}
-	}
+    static void addGraph(int from, int to, int weight){
+        list[from].add(new Node(to, weight));
+    }
 }
-
-/*
- * 다익스트라 PQ 버전
- * 
- * 다익스트라: 특정 정점에서 다른 모든 정점까지의 최단 거리
- * 
- * 1. 비방문 정점 중 최소 비용 정점 선택 2. 그 정점과 연결된 비방문 정점의 최단 거리 갱신. 기존 거리 VS 경유지 거쳐서 가는 거리
- * 
- */
