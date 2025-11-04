c7ff10513aad06ced28d5d90b68c14d0897c0dea
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-11-04 14:53:03 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 588 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,114 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-	
-	static int n;
-	static int[] seq; //수열 
-	static char[] oper; //연산자 
-	static char[] tempOper; //연산자 순열 
-	static boolean[] visited;
-	static int max=Integer.MIN_VALUE;
-	static int min=Integer.MAX_VALUE;
-	static char[] opList= {'+','-','*','/'};
+    static int N;
+    static int[] numbers;
+    static int[] operator;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static char[] op;
+    static boolean[] isSelected;
+    static int sum_op;
+    static char[] result;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        numbers = new int[N];
+        for(int i = 0; i < N; i++){
+            numbers[i] = sc.nextInt();
+        }
+        operator = new int[4];
+        sum_op = 0;
+        for(int i = 0; i < 4; i++){
+            operator[i] = sc.nextInt();
+            sum_op += operator[i];
+        }
+        op = new char[sum_op];
+        result = new char[sum_op];
+        int idx = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < operator[i]; j++){
+                if(i == 0){
+                    op[idx++] = '+'; 
+                } else if(i == 1){
+                    op[idx++] = '-';
+                } else if(i == 2){
+                    op[idx++] = '*';
+                } else {
+                    op[idx++] = '/';
+                }
+            }
+        }
+        isSelected = new boolean[sum_op];
+        // for(int i = 0; i < sum_op; i++){
+        //     System.out.println(op[i]);
+        // }
+        per(0);
+        System.out.println(max);
+        System.out.println(min);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		n=Integer.parseInt(br.readLine());
-		seq=new int[n];
-		oper=new char[n-1];
-		tempOper=new char[n-1];
-		visited=new boolean[n-1];
-		
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++) {
-			seq[i]=Integer.parseInt(st.nextToken());
-		}
-		
-		int[] opCnt=new int[4];
-		st= new StringTokenizer(br.readLine());
-		
-		for(int i=0;i<4;i++) {
-			opCnt[i]=Integer.parseInt(st.nextToken());
-		}
-		
-		int idx=0;
-		for(int i=0;i<4;i++) {
-			int cnt=opCnt[i];
-			char c=opList[i]; //0이면 +, 1이면 -,..
-			
-			for(int j=0;j<cnt;j++) {
-				oper[idx++]=c;
-			}
-		}
-		
-		
-		dfs(0);
-		System.out.println(max);
-		System.out.println(min);
-	}
-	
-	//백트래킹으로 연산자 순열 
-	static void dfs(int depth) {
-		
-		if(depth==n-1) {
-			bruteForce();
-			return;
-		}
-		
-		for(int i=0;i<n-1;i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				tempOper[depth]=oper[i];
-				dfs(depth+1);
-				visited[i]=false;
-			}
-		}
-		
-	}
-	
-	//연산자 순열에 따른 수열 계산 
-	static void bruteForce() {
-		int result=seq[0];
-		
-		for(int i=1;i<n;i++) {
-			int n2=seq[i];
-			int op=tempOper[i-1];
-			
-			switch(op) {
-			
-			case '/':
-				if(result<0) {
-					int temp=-1*result; //양수로 바꾼 뒤 
-					int sh=temp/n2; //몫을 구하고,
-					result=-1*sh; //다시 음수로 바꾼다. 
-				}else {
-					result/=n2;
-				}
-				break;
-				
-			case '+':
-				result+=n2;
-				break;
-			
-			case '-':
-				result-=n2;
-				break;
-				
-			case '*':
-				result*=n2;
-				break;
-			}
-		}
-		
-		//최대, 최소 구하기 
-		min=Math.min(min, result);
-		max=Math.max(max, result);
-	}
+    static void per(int cnt){
+        if(cnt == sum_op){
+            int value = calc();
+            max = Math.max(value, max);
+            min = Math.min(value, min);
+            return ;
+        }
+
+        for(int i = 0; i < sum_op; i++){
+            if(isSelected[i]) continue;
+            isSelected[i] = true;
+            result[cnt] = op[i];
+            per(cnt + 1);
+            isSelected[i] = false;
+        }
+    }
+
+    static int calc(){
+        int res = numbers[0];
+        for(int i = 0; i < sum_op; i++){
+        char c = result[i]; 
+        int nextNum = numbers[i + 1];
+        switch(c){
+            case '+': res += nextNum; break;
+            case '-': res -= nextNum; break;
+            case '*': res *= nextNum; break;
+            case '/': res /= nextNum; break; 
+        }
+    }
+    return res;
+    }
 }
-
-
-
