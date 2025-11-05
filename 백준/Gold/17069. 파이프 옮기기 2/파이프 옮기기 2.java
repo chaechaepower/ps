d7e9b232a738e29b74757b6d45d1375c56385957
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-11-05 13:50:20 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 220 ms, Memory: 20692 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,52 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    static int N;
+    static int[][] map;
+    static long[][][] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        map = new int[N + 1][N + 1];
+        dp = new long[N + 1][N + 1][3];
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+        dp[1][2][0] = 1;
+        for(int i = 1; i <= N; i++){
+            for(int j = 3; j <= N; j++){
+                if(map[i][j] == 1) continue;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(br.readLine());
-		
-		int[][] board=new int[n+1][n+1];
-		StringTokenizer st;
-		
-		for(int i=1;i<n+1;i++) {
-			st=new StringTokenizer(br.readLine());
-			
-			for(int j=1;j<n+1;j++) {
-				board[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		long[][][] dp=new long[n+1][n+1][3]; // 0: 가로 1: 세로 2: 대각선 
-		dp[1][2][0]=1;
-		
-		for(int i=1;i<n+1;i++) {
-			for(int j=1;j<n+1;j++) {
-				if(board[i][j]==1) continue;
-				
-				if(j-1>=0 && board[i][j-1]==0) {
-					dp[i][j][0]+=dp[i][j-1][0]+dp[i][j-1][2];
-				}
-				
-				if(i-1>=0 && board[i-1][j]==0) {
-					dp[i][j][1]+=dp[i-1][j][1]+dp[i-1][j][2];
-				}
-				
-				if(i-1>=0 && j-1>=0 && board[i-1][j]==0 && board[i][j-1]==0 && board[i-1][j-1]==0) {
-					dp[i][j][2]+=dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
-				}
-			}
-		}
-		
-		System.out.println(dp[n][n][0]+dp[n][n][1]+dp[n][n][2]);
-	}
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+
+                if(i == 1) continue;
+
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+                if(map[i - 1][j]  == 1|| map[i][j - 1] == 1) continue;
+
+                dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
+        }
+
+        System.out.println(dp[N][N][0] + dp[N][N][1] + dp[N][N][2]);
+    }
 }
-
-/*
- * dp 배열 채우기 
- * 메모리 크기가 M 이상인 것 중 비용이 최소인 것 
- * */
- 
