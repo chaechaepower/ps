239c12010181ae39ace6e2674baf8433b204f1cb
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2025-11-10 15:23:13 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 12932 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,95 +1,91 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-	static char[][] board;
 	static int n;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+	static char[][] map;
 	static boolean[][] visited;
+	static int[] dr= {-1,1,0,0};
+	static int[] dc= {0,0,-1,1};
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-
-		board = new char[n][n];
-
-		for (int i = 0; i < n; i++) {
-			board[i] = br.readLine().toCharArray();
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(br.readLine());
+		
+		map=new char[n][n];
+		
+		for(int i=0;i<n;i++) {
+			String line=br.readLine();
+			
+			for(int j=0;j<n;j++) {
+				map[i][j]=line.charAt(j);
+			}
 		}
-
-		// 정상
-		int normal = 0;
-		visited = new boolean[n][n];
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (!visited[i][j]) {
-					bfs(i, j, setColors(board[i][j],false));
-					normal++;
+		
+		int colored=0, nonColored=0; // 정상, 색맹
+		
+		visited=new boolean[n][n];
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(!visited[i][j]) {
+					dfsColored(i,j);
+					colored++;
 				}
 			}
 		}
 		
-		int color=0;
-		visited = new boolean[n][n];
+		visited=new boolean[n][n];
 		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (!visited[i][j]) {
-					bfs(i, j, setColors(board[i][j],true));
-					color++;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(!visited[i][j]) {
+					dfsNonColored(i,j);
+					nonColored++;
 				}
 			}
 		}
-
-		System.out.print(normal+" "+color);
+		
+		System.out.printf("%d %d",colored, nonColored);
 	}
 
-	private static void bfs(int i, int j, Set<Character> colorSet) {
-		Queue<int[]> que = new LinkedList<>();
-		que.offer(new int[] { i, j });
-		visited[i][j] = true;
+	private static void dfsColored(int i, int j) {
+		visited[i][j]=true;
+		
+		for(int d=0;d<4;d++) {
+			int nextR=i+dr[d];
+			int nextC=j+dc[d];
+			
+			if(nextR<0 || nextR>=n || nextC<0 || nextC>=n) continue;
+						
+			if(visited[nextR][nextC]) continue;
+			
+			if(map[nextR][nextC]!=map[i][j]) continue;
 
-		while (!que.isEmpty()) {
-			int[] cur = que.poll();
-			int curR = cur[0];
-			int curC = cur[1];
-
-			for (int d = 0; d < 4; d++) {
-				int nr = curR + dr[d];
-				int nc = curC + dc[d];
-
-				if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
-					continue;
-				}
-
-				if (visited[nr][nc]) {
-					continue;
-				}
-
-				if (colorSet.contains(board[nr][nc])) {
-					visited[nr][nc] = true;
-					que.offer(new int[] { nr, nc });
-				}
-			}
+			dfsColored(nextR, nextC);
 		}
 	}
+	
+	private static void dfsNonColored(int i, int j) {
+		visited[i][j]=true;
+		
+		for(int d=0;d<4;d++) {
+			int nextR=i+dr[d];
+			int nextC=j+dc[d];
+			
+			if(nextR<0 || nextR>=n || nextC<0 || nextC>=n) continue;
+						
+			if(visited[nextR][nextC]) continue;
+			
+			if((map[i][j]!='B'&&map[nextR][nextC]=='B') || (map[i][j]=='B'&&map[nextR][nextC]!='B')) continue;
 
-	private static Set<Character> setColors(char color, boolean colored) {
-		Set<Character> colorSet = new HashSet<>();
-		colorSet.add(color);
-
-		if (colored) {
-			if(color=='R') colorSet.add('G');
-			if(color=='G') colorSet.add('R');
+			dfsNonColored(nextR, nextC);
 		}
-
-		return colorSet;
 	}
 }
