58631fc31ec0285a17715c685ab5e28301387499
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-06-19 10:42:07 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,28 @@
 class Solution {
     boolean[] visited;
     
-    public int solution(int n, int[][] computers) {
-        visited=new boolean[n];
-        
-        int count=0;
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i, n, computers);
-                count++;
-            }
-        }
-        
-        return count;
-    }
+	public int solution(int n, int[][] computers) {
+		visited = new boolean[n];
+
+		int answer = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				dfs(i, computers);
+				answer++;
+			}
+		}
+
+		return answer;
+
+	}
     
-    void dfs(int v, int n, int[][] computers){
-        for(int i=0;i<n;i++){
-            if(i!=v && !visited[i] && computers[v][i]==1){
-                visited[i]=true;
-                dfs(i, n, computers);
+    public void dfs(int x, int[][] arr) {
+        visited[x] = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i] && arr[x][i] == 1) {
+                dfs(i, arr);
             }
         }
     }
