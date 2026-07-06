62fcbeaf05a0585a96f9afd807d7cd97738ac393
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-07-06 10:41:18 +0900

    [level 2] Title: 타겟 넘버, Time: 2.93 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,50 +1,22 @@
-import java.util.*;
-
 class Solution {
-    char[] opList;
-    int[] numberList;
-    int len;
-    int targetNumber;
-    int count;
-    
-    public int solution(int[] numbers, int target) {
-        len=numbers.length;
-        opList=new char[len];
-        numberList=Arrays.copyOf(numbers,len);
-        targetNumber=target;
-        dfs(0);
-        return count;
-    }
-    
-    public void dfs(int depth){
-        if(depth==len){
-            calculateTargetNumber();
-            return;
-        }
-        
-        for(int i=0;i<2;i++){
-            opList[depth]="+-".charAt(i);
-            dfs(depth+1);
-        }
-    }
-    
-    public void calculateTargetNumber(){
-        int result=0;
-        
-        for(int i=0;i<len;i++){
-            char op=opList[i];
-            
-            if(op=='+'){
-                result+=numberList[i];
-            }
-            
-            if(op=='-'){
-                result-=numberList[i];
-            }
-        }
-        
-        if(result==targetNumber){
-            count++;
-        }
-    }
+	int n;
+	int cnt;
+
+	public int solution(int[] numbers, int target) {
+		n = numbers.length;
+		dfs(0, 0, numbers, target);
+		return cnt;
+	}
+
+	private void dfs(int idx, int sum, int[] numbers, int target) {
+		if (idx == n) {
+			if (sum == target) {
+				cnt++;
+			}
+			return;
+		}
+
+		dfs(idx + 1, sum + numbers[idx], numbers, target);
+		dfs(idx + 1, sum - numbers[idx], numbers, target);
+	}
 }
