e3ac349e8000a023eb72b1a654da928fdb3e8b45
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-07-23 12:47:21 +0900

    [level 2] Title: 구명보트, Time: 12.73 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,30 +2,28 @@ import java.util.Arrays;
 
 class Solution {
 	public int solution(int[] people, int limit) {
-
-		int answer=0;
-		
 		Arrays.sort(people);
-
-		int left = 0;
-		int right = people.length - 1;
 		
-		while(left<right) {
-			if(people[left]+people[right]<=limit) {
+		int l = 0, r = people.length - 1;
+
+		int answer = 0;
+
+		while (l < r) {
+			if (people[l] + people[r] <= limit) {
 				answer++;
-				left++;
-				right--;
-			}
+				l++;
+				r--;
+			} 
 			else {
 				answer++;
-				right--;
+				r--;
 			}
 		}
-		
-		if(left==right) {
+
+		if (l == r) {
 			answer++;
 		}
-
+		
 		return answer;
 	}
 }
