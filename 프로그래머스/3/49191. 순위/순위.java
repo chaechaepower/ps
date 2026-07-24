ee084a26090ab2c7db9f7e5bbd227f5118d10e1b
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-07-25 00:06:26 +0900

    [level 3] Title: 순위, Time: 6.71 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,66 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 class Solution {
-	List<Integer>[] win;
-	List<Integer>[] lose;
-	boolean[] visited;
 
 	public int solution(int n, int[][] results) {
-		win = new ArrayList[n + 1]; // 0사용 x
-		for (int i = 1; i < n + 1; i++) {
-			win[i] = new ArrayList<>();
+		boolean[][] graph=new boolean[n+1][n+1];
+		
+		for(int[] result:results) {
+			int win=result[0];
+			int lose=result[1];
+			
+			graph[win][lose]=true;
 		}
-
-		lose = new ArrayList[n + 1]; // 0사용 x
-		for (int i = 1; i < n + 1; i++) {
-			lose[i] = new ArrayList<>();
-		}
-
-		for (int[] result : results) {
-			int u = result[0];
-			int v = result[1];
-
-			win[u].add(v);
-			lose[v].add(u);
-		}
-
-		// 정방향 dfs
-		int[] winCnt = new int[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			visited = new boolean[n + 1];
-			winCnt[i] = dfs(i, win);
-		}
-
-		// 역방향 dfs
-		int[] loseCnt = new int[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			visited = new boolean[n + 1];
-			loseCnt[i] = dfs(i, lose);
-		}
-
-		int answer = 0;
-		for (int i = 1; i < n + 1; i++) {
-			if (winCnt[i] + loseCnt[i] == n - 1) {
-				answer++;
+	
+		for(int k=1;k<=n;k++) {
+			for(int i=1;i<=n;i++) {
+				for(int j=1;j<=n;j++) {
+										
+					if(graph[i][k] && graph[k][j]) {
+						graph[i][j]=true;
+					}
+				}
 			}
 		}
+		
+		int answer=0;
+		
+		for(int i=1;i<=n;i++) {
+			int cnt=0;
 
+			for(int j=1;j<=n;j++) {
+				if(graph[i][j] || graph[j][i]) cnt++;
+			}
+			
+			if(cnt==n-1) answer++;
+		}
+		
+		
 		return answer;
 	}
-
-	public int dfs(int now, List<Integer>[] graph) {
-		visited[now] = true;
-
-		int cnt = 0;
-
-		for (int next : graph[now]) {
-			if (visited[next])
-				continue;
-
-			cnt += 1 + dfs(next, graph);
-		}
-
-		return cnt;
-	}
 }
+	
