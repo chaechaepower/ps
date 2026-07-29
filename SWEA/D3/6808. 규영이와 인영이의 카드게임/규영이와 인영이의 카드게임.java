4bbbdbb0026687f40c8922630553e6fb62247302
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-07-29 15:48:10 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,286 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,85 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class Solution {
-    static List<Integer> inCards;
-    static List<Integer> gyoCards;
-    static boolean[] visited;
-    static List<Integer> randInCards;
-    static int inwin;
-    static int inLose;
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+	static int[] in, gyo, result;
+	static boolean[] visited;
+	static int answer;
 
-        int testN=Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testN = Integer.parseInt(br.readLine());
 
-        for(int t=1;t<=testN;t++){
-            inCards=new ArrayList<>();
-            gyoCards=new ArrayList<>();
+		for (int t = 1; t <= testN; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-            StringTokenizer st=new StringTokenizer(br.readLine());
-            
-            for(int i=0;i<9;i++){
-                gyoCards.add(Integer.parseInt(st.nextToken()));
-            }
+			// 규영 카드 초기화
+			gyo = new int[9];
+			boolean[] check = new boolean[19]; // 1~18
 
-            for(int i=1;i<19;i++){
-                if(gyoCards.contains(i)){
-                    continue;
-                }
-                inCards.add(i);
-            }
+			for (int i = 0; i < 9; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				gyo[i] = num;
+				check[num] = true;
+			}
 
-            randInCards=new ArrayList<>();
-            visited=new boolean[9];
-            inwin=0; inLose=0;
-            dfs(0,0);
+			// 인영 카드 초기화
+			in = new int[9];
+			for (int i = 1, idx = 0; i <= 18; i++) {
+				if (check[i])
+					continue;
 
-            System.out.printf("#%d %d %d\n",t,inLose,inwin);
-        }
-    }
+				in[idx++] = i;
+			}
 
-    public static void dfs(int v, int depth){
-        if(depth==9){
-            game();
-            return;
-        }
+			visited = new boolean[9];
+			result=new int[9];
+			
+			answer=0;
+			perm(0);
+			System.out.printf("#%d %d %d\n", t, answer, per(9)-answer);
+		}
 
-        for(int i=0;i<9;i++){
-            if(!visited[i]){
-                visited[i]=true;
-                randInCards.add(inCards.get(i));
-                dfs(i,depth+1);
-                randInCards.remove(randInCards.size()-1);
-                visited[i]=false;
+	}
 
-            }
-        }
-    }
+	public static void perm(int depth) {
+		if (depth == 9) {
+			if (isGyoWin())
+				answer++;
+			return;
+		}
 
-    public static void game(){
-        int gyoSum=0;
-        int inSum=0;
+		for (int i = 0; i < 9; i++) {
+			if (visited[i])
+				continue;
 
-        for(int i=0;i<9;i++){
-            if(randInCards.get(i)>gyoCards.get(i)){
-                inSum+=randInCards.get(i)+gyoCards.get(i);
-            }
-            else{
-                gyoSum+=randInCards.get(i)+gyoCards.get(i);
-            }
-        }
+			visited[i] = true;
+			result[depth] = in[i];
 
-        if(inSum>gyoSum){
-            inwin++;
-        } 
-        else{
-            inLose++;
-        }
-    }    
+			perm(depth + 1);
+
+			visited[i] = false;
+		}
+	}
+
+	public static boolean isGyoWin() {
+		int inSum = 0, gyoSum = 0;
+
+		for (int i = 0; i < 9; i++) {
+			if (gyo[i] > result[i]) {
+				gyoSum += gyo[i] + result[i];
+			} else {
+				inSum += gyo[i] + result[i];
+			}
+		}
+
+		return gyoSum > inSum;
+	}
+
+	public static int per(int n) {
+		if (n == 1)
+			return 1;
+
+		return n * per(n - 1);
+	}
 }
+
+/*
+ * 모든 순열을 구한다. 각 경우마다 계산한다
+ * 
+ */
