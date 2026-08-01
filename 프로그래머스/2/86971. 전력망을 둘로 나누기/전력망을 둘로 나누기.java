5de9528651638f162702da30052e72fe1b6a084f
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-01 15:43:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.03 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,72 +1,54 @@
-import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    int[][] map; //인접 행렬
-    boolean[] visited;
-    int ans=Integer.MAX_VALUE;
-    int cnt;
-    
-    public int solution(int n, int[][] wires) {
-        map=new int[n+1][n+1]; //0은 안씀
-        
-        for(int i=0;i<wires.length;i++){ //간선 정보 저장
-            int v1=wires[i][0];
-            int v2=wires[i][1];
-            
-            map[v1][v2]=1;
-            map[v2][v1]=1;
-        }
+	List<Integer>[] graph;
+	boolean[] visited;
+	int count;
+	int answer = Integer.MAX_VALUE;
 
-        
-        for(int i=0;i<wires.length;i++){
-            int v1=wires[i][0];
-            int v2=wires[i][1];
-            
-            map[v1][v2]=0;
-            map[v2][v1]=0;
-            
-            visited=new boolean[n+1]; 
-            
-            int diff=0;
-            for(int v=1;v<=n;v++){
-                if(!visited[v]){
-                    cnt=0;
-                    dfs(v,n);
-                    System.out.println(cnt);
-                    if(v==1){
-                        diff=cnt;
-                    }else{
-                        diff=Math.abs(cnt-diff);
-                    }
-                }
-            }
-            System.out.println(diff);
-            System.out.println();
-            ans=Math.min(diff,ans);
-            
-            map[v1][v2]=1;
-            map[v2][v1]=1;
-        }
-        
-        return ans;
+	public int solution(int n, int[][] wires) {
 
-    }
-    
-    void dfs(int v, int n){
-        visited[v]=true;
-        cnt++;
-        
-        for(int i=1;i<=n;i++){
-            if(!visited[i] && map[v][i]==1 && map[i][v]==1){
-                dfs(i,n);
-            }
-        }
-    }
+		graph = new ArrayList[n + 1];
+		for (int j = 1; j < n + 1; j++) {
+			graph[j] = new ArrayList<>();
+		}
+
+		for (int j = 0; j < wires.length; j++) {
+			int u = wires[j][0];
+			int v = wires[j][1];
+
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+
+		for (int i = 0; i < wires.length; i++) {
+			int u = wires[i][0]; // 제외
+			int v = wires[i][1];
+
+			visited = new boolean[n + 1];
+
+			count = 0;
+			dfs(1, u, v);
+			answer = Math.min(answer, Math.abs(count - (n - count)));
+		}
+
+		return answer;
+	}
+
+	public void dfs(int now, int u, int v) {
+		visited[now] = true;
+		count++;
+
+		for (int next : graph[now]) {
+			if (visited[next])
+				continue;
+			
+			if((now==u && next==v) || (now==v && next==u)) {
+				continue;
+			}
+
+			dfs(next, u, v);
+		}
+	}
 }
-
-/*
-인접 행렬 또는 인접 리스트로 그래프 정보 나타냄.
-간선을 하나 없애고 그 경우에 대해 dfs를 실행한 뒤 노드 개수 차이 계산
-노드 개수 차이가 최대일 때를 구하자.
-
-*/
