268b6ef031129b9d4f59a22f27433cd6e513fb33
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-05 00:55:41 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,786 ms, Memory: 28,436 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,69 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
-	static int[][] clients;
-	static boolean[] visited;
-	static int min;
+class Solution {
 	static int n;
-	static int[] business, home;
+	static int[] company, home;
+	static int[][] client;
+	static boolean[] visited;
+	static int[] result;
+	static int minDis;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testN = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= testN; t++) {
-			n = Integer.parseInt(br.readLine());
-
-			business = new int[2];
-			home = new int[2];
+			n = Integer.parseInt(br.readLine()); // 고객의 개수
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			business[0] = Integer.parseInt(st.nextToken());
-			business[1] = Integer.parseInt(st.nextToken());
+			company = new int[2];
+			home = new int[2];
+
+			company[0] = Integer.parseInt(st.nextToken());
+			company[1] = Integer.parseInt(st.nextToken());
 			home[0] = Integer.parseInt(st.nextToken());
 			home[1] = Integer.parseInt(st.nextToken());
 
-			clients = new int[n][2];
+			client=new int[n][2];
+			
 			for (int i = 0; i < n; i++) {
-				clients[i][0] = Integer.parseInt(st.nextToken());
-				clients[i][1] = Integer.parseInt(st.nextToken());
+				client[i][0] = Integer.parseInt(st.nextToken());
+				client[i][1] = Integer.parseInt(st.nextToken());
 			}
-			
-			visited=new boolean[n];
-			min=Integer.MAX_VALUE;
-			dfs(0,business,0);
-			
-			System.out.printf("#%d %d\n",t,min);
+
+			visited = new boolean[n];
+			result = new int[n];
+			minDis = Integer.MAX_VALUE;
+			perm(0);
+			System.out.printf("#%d %d\n", t, minDis);
 		}
 	}
 
-	public static void dfs(int depth, int[] prev, int total) {
-		if(total>min) return;
-		
-		if(depth==n) {
-			total+=Math.abs(prev[0]-home[0]) + Math.abs(prev[1]-home[1]);
-			min=Math.min(min, total);
+	private static void perm(int depth) {
+		if (depth == n) {
+			minDis = Math.min(minDis, cal());
 			return;
 		}
-		
-		for(int i=0;i<n;i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				int diff=Math.abs(prev[0]-clients[i][0]) + Math.abs(prev[1]-clients[i][1]);
-				dfs(depth+1, clients[i], total+diff);
-				visited[i]=false;
-			}
+
+		for (int i = 0; i < n; i++) {
+			if (visited[i])
+				continue;
+
+			visited[i] = true;
+			result[depth] = i;
+			perm(depth + 1);
+			visited[i] = false;
 		}
 	}
-}
 
-/*
- * 집 방문 순서 이동 거리가 최단 이동 거리보다 크면 바로 return 이전 위치 저장
- * 
- * 
- */
+	private static int cal() {
+		int dis = 0;
+
+		// 회사~ 고객 1
+		int idx=result[0];
+		dis += Math.abs(company[0] - client[idx][0]) + Math.abs(company[1] - client[idx][1]);
+
+		// 고객
+		for(int i=0;i<n-1;i++) {
+			int u=result[i];
+			int v=result[i+1];
+			dis += Math.abs(client[u][0] - client[v][0]) + Math.abs(client[u][1] - client[v][1]);
+		}
+
+		// 막 고객 ~ 집
+		idx=result[n-1];
+		dis += Math.abs(home[0] - client[idx][0]) + Math.abs(home[1] - client[idx][1]);
+
+		return dis;
+	}
+}
