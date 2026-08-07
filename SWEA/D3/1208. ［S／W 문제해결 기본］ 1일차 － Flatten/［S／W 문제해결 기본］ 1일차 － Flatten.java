bfa2af8f000ab386d6398af4afdffe3a4be1b13d
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-07 15:04:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 98 ms, Memory: 28,376 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,66 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Solution {
+class Solution {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int t=1;t<=10;t++) {
-			int dumpN=Integer.parseInt(br.readLine());
-			
-			StringTokenizer st=new StringTokenizer(br.readLine());
-			int[] height=new int[101];
-			
-			int min=Integer.MAX_VALUE;
-			int max=Integer.MIN_VALUE;
-			
-			for(int i=0;i<100;i++) {
-				int num=Integer.parseInt(st.nextToken());
-				height[num]++;
-				max=Math.max(max, num);
-				min=Math.min(min, num);
-			}
-						
-			while(dumpN-->0) {
-				height[max]--;
-				height[max-1]++;
-				height[min]--;
-				height[min+1]++;
 
-				if(height[max]==0){
-					for(int i=max-1;i>=0;i--) {
-						if(height[i]>0) {
-							max=i;
-							break;
-						}
-					}
-				}
-				
-				if(height[min]==0){
-					for(int i=min+1;i<101;i++) {
-						if(height[i]>0) {
-							min=i;
-							break;
-						}
-					}
-				}
-				
+		for (int t = 1; t <= 10; t++) {
+			int dump = Integer.parseInt(br.readLine());
+
+			int[] arr = new int[100];
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 100; i++) {
+				arr[i]=Integer.parseInt(st.nextToken());
 			}
-		
 			
+			Arrays.sort(arr);
+
+			for (int i = 0; i < dump; i++) {
+				arr[99]=arr[99]-1;
+				arr[0]=arr[0]+1;
+				Arrays.sort(arr);
+			}
+
+			int max = arr[99];
+			int min = arr[0];
 			
-			System.out.println("#"+t+" "+(max-min));
+			System.out.printf("#%d %d\n", t, max - min);
 		}
 	}
-
-	
 }
