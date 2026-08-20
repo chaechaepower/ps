8ea4637f333e6f48404f8ca926bc38d675fb1df9
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-20 15:09:34 +0900

    [level 3] Title: 베스트앨범, Time: 5.25 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -4,47 +4,61 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 class Solution {
-	public int[] solution(String[] genres, int[] plays) {
-		Map<String, List<Integer>> map=new HashMap<>(); // <장르, 고유번호>
-		Map<String, Integer> total = new HashMap<>();
+    
+    	static class Music{
+		public int idx;
+		public int plays;
 		
-        for(int i=0;i<genres.length;i++) {
-        	map.computeIfAbsent(genres[i], k->new ArrayList<>()).add(i);
-        	total.put(genres[i], total.getOrDefault(genres[i], 0)+plays[i]);
-        }
-        
-        List<String> list= new ArrayList<>(total.keySet());
-        Collections.sort(list, (l1,l2)-> total.get(l2)-total.get(l1)); // 장르 재생수 내림차순 
-        
-        List<Integer> result=new ArrayList<>();
-        
-        for(String genre:list) {
-        	List<Integer> indexs=map.get(genre);
-        	
-        	Collections.sort(indexs,(i1,i2)->{
-        		if(plays[i1]==plays[i2]) { // 재생수 같으면 고유번호 낮은 순 
-        			return i1-i2;
-        		}
-        		return plays[i2]-plays[i1]; // 재생수 순
-        	});
-        	
-        	if(indexs.size()==1) {
-        		result.add(indexs.get(0));
-        		continue;
-        	}
-        	
-        	result.add(indexs.get(0));
-        	result.add(indexs.get(1));
-        }
-        
-        int[] answer=new int[result.size()];
-        
-        for(int i=0;i<result.size();i++) {
-        	answer[i]=result.get(i);
-        }
-        
-        return answer;
+		public Music(int idx, int plays) {
+			this.idx = idx;
+			this.plays = plays;
+		}
+	}
+    
+		public int[] solution(String[] genres, int[] plays) {
+		Map<String, List<Music>> map=new HashMap<>();
+		Map<String, Integer> total=new HashMap<>();
+		
+		for(int i=0;i<genres.length;i++) {
+			Music music=new Music(i, plays[i]);
+			map.computeIfAbsent(genres[i], k->new ArrayList<>()).add(music);
+			
+			total.put(genres[i], total.getOrDefault(genres[i], 0)+plays[i]);
+		}
+		
+		// 1. 총 재생수 기준 장르 내림차순  
+		List<String> musics=new ArrayList<>(total.keySet());
+		Collections.sort(musics,(s1,s2)-> total.get(s2)-total.get(s1));
+		
+		// 2. 가장 많이 재생된 장르중 가장 많이 재생된 노래 선택 (같으면 고유 번호 낮을거 선택)
+		List<Integer> result=new ArrayList<>();
+		
+		for(String music:musics) {
+			List<Music> list=map.get(music);
+			
+			if(list.size()==1) {
+				result.add(list.get(0).idx);
+				continue;
+			}
+			
+			Collections.sort(list,(m1,m2)->{
+				if(m1.plays==m2.plays) {
+					return m1.idx-m2.idx;
+				}
+				
+				return m2.plays-m1.plays;
+			});
+			
+			result.add(list.get(0).idx);
+			result.add(list.get(1).idx);
+		}
+		
+		int[] answer=new int[result.size()];
+		for(int i=0;i<result.size();i++) {
+			answer[i]=result.get(i);
+		}
+		
+		return answer;
     }
 }
