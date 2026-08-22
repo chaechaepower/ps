616b1b3f5ca39943a00bd5e9547c5d445890b248
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-22 16:21:11 +0900

    [level 2] Title: 주식가격, Time: 12.94 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
-public int[] solution(int[] prices) {
-    	int len= prices.length;
-        int[] result=new int[len];
-        
-        for(int i=0;i<len-1;i++) {
-        	int now=prices[i];
-        	int count=0;
-        	
-        	for(int j=i+1;j<len;j++) {
-        		int post=prices[j];
-        		count++;
-        		
-        		if(now>post) {
-        			break;
-        		}
-        	}
-        	
-        	result[i]=count;
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] result = new int[len];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < len; i++) {
+
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+
+            stack.push(i);
         }
-        
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            result[idx] = len - 1 - idx;
+        }
+
         return result;
     }
 }
