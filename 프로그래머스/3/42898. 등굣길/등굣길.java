24f16f568c4b5c5bb1535f0d7cc2d98d5016b62a
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-25 14:05:55 +0900

    [level 3] Title: 등굣길, Time: 0.82 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,35 +1,47 @@
+import java.util.Arrays;
+
 class Solution {
-	static final int MOD = 1_000_000_007;
-	int[][] dp; // n x m
+	int[][] arr; // 1: 갈 수 있음 0: 물 웅덩이
+	int[][] dp;
 
 	public int solution(int m, int n, int[][] puddles) {
-		int answer = 0;
+		arr = new int[m + 1][n + 1];
+		for (int i = 1; i < m + 1; i++) {
+			Arrays.fill(arr[i], 1);
+		}
+
+		for (int i = 0; i < puddles.length; i++) {
+			int r = puddles[i][0];
+			int c = puddles[i][1];
+
+			arr[r][c] = 0;
+		}
+
 		dp = new int[m + 1][n + 1];
+		for (int i = 1; i < m + 1; i++) {
+			Arrays.fill(dp[i], -1);
+		}
 
 		dp[1][1] = 1;
 
-		for (int i = 0; i < puddles.length; i++) {
-			int x = puddles[i][0];
-			int y = puddles[i][1];
-			dp[x][y] = -1;
+		return recur(m, n);
+	}
+
+	private int recur(int r, int c) {
+		if (r < 1 || c < 1) {
+			return 0;
 		}
 
-		for (int x = 1; x <= m; x++) {
-			for (int y = 1; y <= n; y++) {
-				if(x==1 && y==1) {
-					continue;
-				}
-				
-				if (dp[x][y] == -1) {
-					dp[x][y] = 0;
-					continue;
-				}
-
-				dp[x][y] = (dp[x - 1][y] + dp[x][y - 1]) % MOD;
-			}
+		if (arr[r][c] == 0) {
+			return 0;
 		}
 
-		answer = dp[m][n];
-		return answer;
+		if (dp[r][c] != -1) {
+			return dp[r][c];
+		}
+
+		dp[r][c] = (recur(r - 1, c) + recur(r, c - 1)) % 1_000_000_007;
+
+		return dp[r][c];
 	}
 }
