eba98f29222cd86b825842b00b262268d38dc122
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-08-25 14:26:34 +0900

    [D3] Title: 0/1 Knapsack, Time: 112 ms, Memory: 30,592 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int n, k;
+	static int[] volumes, credits;
+	static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,28 +15,46 @@ public class Solution {
 
 		for (int t = 1; t <= testN; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n=Integer.parseInt(st.nextToken());
-			int k=Integer.parseInt(st.nextToken());
-			
-			int[] volumes=new int[n+1];
-			int[] costs=new int[n+1];
-			
-			for(int i=1;i<n+1;i++) {
+			n = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+
+			volumes = new int[n + 1];
+			credits = new int[n + 1];
+
+			for (int i = 1; i <= n; i++) {
 				st = new StringTokenizer(br.readLine());
-				volumes[i]=Integer.parseInt(st.nextToken());
-				costs[i]=Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+
+				volumes[i] = v;
+				credits[i] = c;
 			}
-			
-			int[][] dp=new int[n+1][k+1]; //dp[i][j]: 물건을 1~i번까지 고려하여 무게 j를 만드는 최대 가치 수 
-			
-			for(int i=1;i<n+1;i++) {
-				for(int j=1;j<k+1;j++) {
-					if(j<volumes[i]) dp[i][j]=dp[i-1][j];
-					else dp[i][j]=Math.max(dp[i-1][j-volumes[i]]+costs[i], dp[i-1][j]);
-				}
+
+			dp = new int[n + 1][k + 1]; // i번째 물건까지 확인, 최대 수용 k
+			for (int i = 1; i < n + 1; i++) {
+				Arrays.fill(dp[i], -1);
 			}
-			
-			System.out.printf("#%d %d\n",t,dp[n][k]);
+
+			System.out.printf("#%d %d\n", t, recur(n, k));
 		}
+	}
+
+	private static int recur(int i, int k) {
+		if (i < 0) {
+			return 0;
+		}
+
+		if (dp[i][k] == -1) {
+			if (volumes[i] > k) {
+				return recur(i - 1, k);
+			}
+
+			else {
+				dp[i][k] = Math.max(recur(i - 1, k), recur(i - 1, k - volumes[i]) + credits[i]);
+			}
+
+		}
+
+		return dp[i][k];
 	}
 }
