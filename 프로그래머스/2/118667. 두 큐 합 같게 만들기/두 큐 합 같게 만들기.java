7aed7ec22649fcf3ac174ebe84eb7b2b719831c5
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-09-19 16:09:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 107.17 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,56 +1,62 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Solution {
+class Solution{
 
-	public int solution(int[] queue1, int[] queue2) {
-		long sum1 = 0, sum2 = 0;
-
+public int solution(int[] queue1, int[] queue2) {
 		Queue<Integer> q1 = new LinkedList<>();
 		Queue<Integer> q2 = new LinkedList<>();
 
-		for (int i = 0; i < queue1.length; i++) {
-			sum1 += queue1[i];
-			sum2 += queue2[i];
+		long total = 0, sum1 = 0, sum2 = 0;
 
-			q1.offer(queue1[i]);
-			q2.offer(queue2[i]);
+		for (int e : queue1) {
+			q1.offer(e);
+			sum1 += e;
 		}
 
-		if ((sum1 + sum2) % 2 != 0)
+		for (int e : queue2) {
+			q2.offer(e);
+			sum2 += e;
+		}
+    
+        total = sum1 + sum2;
+		
+		if(total%2!=0) {
 			return -1;
-
-		long target = (sum1 + sum2) / 2;
-		int move = 0, cnt = 0;
-
-		while (move <= 4 * queue1.length) {
-			if(sum1==target) {
-				return cnt;
-			}
-			
-			if(sum1<target) {
-				int v=q2.poll();
-				q1.offer(v);
-				
-				sum2-=v;
-				sum1+=v;
-				
-				move++;
-				cnt++;
-			}
-			
-			else if(sum2<target) {
-				int v=q1.poll();
-				q2.offer(v);
-				
-				sum1-=v;
-				sum2+=v;
-				
-				move++;
-				cnt++;
-			}
 		}
 		
-		return -1;
+		total/=2;
+
+		int count = 0;
+
+		while (count < (queue1.length + queue2.length)*4) {
+
+			if (sum1 == sum2) {
+				break;
+			}
+
+			else if (sum1 > sum2) {
+				int out = q1.poll();
+				q2.offer(out);
+				sum1 -= out;
+				sum2 += out;
+				count++;
+			}
+
+			else { // sum2>sum1
+				int out = q2.poll();
+				q1.offer(out);
+				sum2 -= out;
+				sum1 += out;
+				count++;
+			}
+		}
+
+		// 결과
+		if (sum1 == total) {
+			return count;
+		} else {
+			return -1;
+		}
 	}
-}
+    }
