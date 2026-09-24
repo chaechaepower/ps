b0936ad33b142cc34dd6b95fbfb66b9e4352a4b7
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-09-24 22:35:34 +0900

    [level 3] Title: 순위, Time: 5.96 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,21 +1,24 @@
-
+import java.io.IOException;
 
 class Solution {
 
-	public int solution(int n, int[][] results) {
+	public static void main(String[] args) throws IOException {
+	}
+
+	public static int solution(int n, int[][] results) {
 		boolean[][] graph=new boolean[n+1][n+1];
 		
-		for(int[] result:results) {
-			int win=result[0];
-			int lose=result[1];
+		for(int i=0;i<results.length;i++) {
+			int win=results[i][0];
+			int lose=results[i][1];
 			
 			graph[win][lose]=true;
 		}
-	
-		for(int k=1;k<=n;k++) {
-			for(int i=1;i<=n;i++) {
-				for(int j=1;j<=n;j++) {
-										
+		
+		for(int k=1;k<n+1;k++) {
+			for(int i=1;i<n+1;i++) {
+				for(int j=1;j<n+1;j++) {
+					
 					if(graph[i][k] && graph[k][j]) {
 						graph[i][j]=true;
 					}
@@ -25,18 +28,26 @@ class Solution {
 		
 		int answer=0;
 		
-		for(int i=1;i<=n;i++) {
+		for(int i=1;i<n+1;i++) {
 			int cnt=0;
-
-			for(int j=1;j<=n;j++) {
-				if(graph[i][j] || graph[j][i]) cnt++;
+			
+			for(int j=1;j<n+1;j++) {
+				if(graph[i][j] || graph[j][i]) {
+					cnt++;
+				}
 			}
 			
-			if(cnt==n-1) answer++;
+			if(cnt==n-1) {
+				answer++;
+			}
 		}
-		
 		
 		return answer;
 	}
 }
-	
+
+/*
+ * 
+ * 
+ * */
+ 
