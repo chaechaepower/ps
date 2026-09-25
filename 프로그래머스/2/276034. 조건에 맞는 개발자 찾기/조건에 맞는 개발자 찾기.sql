011523dc97b813b8f6876bb8f0ac234f91c03b88
chaechaepower <109583898+chaechaepower@users.noreply.github.com>
2026-09-25 21:36:32 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,16 +1,10 @@
 -- 코드를 작성해주세요
 
-
---  Python 또는 C#에 해당하는 코드랑 각각 & 연산했을 때 그 코드가 나오면 있는 거
-
-
-
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS
-WHERE SKILL_CODE & (SELECT CODE
-                    FROM SKILLCODES B
-                    WHERE NAME='C#')
-OR SKILL_CODE & (SELECT CODE
-                    FROM SKILLCODES B
-                    WHERE NAME='Python')
-ORDER BY ID
+WHERE SKILL_CODE & (
+     SELECT SUM(CODE)
+     FROM SKILLCODES
+     WHERE NAME IN ('Python', 'C#')
+) > 0
+ORDER BY ID;
